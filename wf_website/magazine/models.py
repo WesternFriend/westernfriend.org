92815672276b7f9923b70bafb909060f63f0db83
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-11-02 22:42:03 +0200

    Add Magazine Article search fields and content panels
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- wf_website/magazine/models.py
+++ wf_website/magazine/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class MagazineIndexPage(Page):
@@ -19,3 +20,11 @@ class MagazineIssue(Page):
 
 class MagazineArticle(Page):
     body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
