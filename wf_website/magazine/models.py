9d8a1ce042aeb00a5bd726635f7d71a172ae750e
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-11-02 21:55:48 +0200

    Initial magazine index page
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- wf_website/magazine/models.py
+++ wf_website/magazine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class MagazineIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
