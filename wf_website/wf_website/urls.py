fcf006f45fa2eeb173ba1618b1ec0dcc6c7b30c0
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-11-25 23:34:31 +0200

    Add django_registration URLs
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- wf_website/wf_website/urls.py
+++ wf_website/wf_website/urls.py
@@ -11,6 +11,7 @@ from search import views as search_views
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
