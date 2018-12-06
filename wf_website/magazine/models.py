8c6e386c5d00202dbf2ebd9c4b6dde667bd6daaa
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-06 21:09:42 +0200

    Move Author code to own app
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- wf_website/magazine/models.py
+++ wf_website/magazine/models.py
@@ -3,11 +3,10 @@ from datetime import timedelta
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
-from django.template.defaultfilters import slugify
-from django_extensions.db.fields import AutoSlugField
+
 
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from modelcluster.models import ClusterableModel
+
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from taggit.models import TaggedItemBase
 
@@ -145,7 +144,7 @@ class MagazineTagIndexPage(Page):
 
 class MagazineArticle(Page):
     authored_by = ParentalManyToManyField(
-        'Author',
+        'author.Author',
         related_name='articles',
     )
     body = RichTextField(blank=True)
@@ -166,7 +165,7 @@ class MagazineArticle(Page):
         FieldPanel('body', classname="full"),
         AutocompletePanel(
             'authored_by',
-            page_type='magazine.Author',
+            page_type='author.Author',
             is_single=False,
         ),
         MultiFieldPanel([
@@ -220,43 +219,3 @@ class MagazineIssueFeaturedArticle(Orderable):
         PageChooserPanel('article')
     ]
 
-
-class Author(index.Indexed, ClusterableModel):
-    given_name = models.CharField(
-        max_length=255,
-        default='',
-        help_text="Enter the given name for a person. This can also be used for an organization name."
-    )
-    family_name = models.CharField(
-        max_length=255,
-        blank=True,
-        default='',
-    )
-    slug = AutoSlugField(
-        null=True,
-        blank=True,
-        populate_from=[
-            'given_name',
-            'family_name',
-        ]
-    )
-
-    autocomplete_search_field = 'given_name'
-
-    def autocomplete_label(self):
-        return self.__str__()
-
-    def __str__(self):
-        return f"{self.given_name} {self.family_name}"
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.__str__())
-        super(Author, self).save(*args, **kwargs)
-
-    class Meta:
-        db_table = 'author'
-
-    search_fields = [
-        index.SearchField('given_name', partial_match=True),
-        index.SearchField('family_name', partial_match=True),
-    ]
