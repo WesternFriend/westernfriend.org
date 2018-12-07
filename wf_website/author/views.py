1672fc9630a979a586a00fb76820fb7862cee8e8
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-12-07 22:23:12 +0200

    Cleanup
    Signed-off-by: Brylie Christopher Oxley <brylie@gnumedia.org>

--- wf_website/author/views.py
+++ wf_website/author/views.py
@@ -5,8 +5,3 @@ from .models import Author
 
 class AuthorDetail(DetailView):
     model = Author
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        return context
