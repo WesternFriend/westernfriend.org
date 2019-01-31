3e6dbc75713de74e09948ff941ba14ee26da4ea8
Brylie Christopher Oxley <brylie@amble.fi>
2019-01-31 20:42:06 +0200

    Lint
    Signed-off-by: Brylie Christopher Oxley <brylie@amble.fi>

--- wf_website/magazine/models.py
+++ wf_website/magazine/models.py
@@ -3,21 +3,18 @@ from datetime import timedelta
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
-
-
 from modelcluster.contrib.taggit import ClusterTaggableManager
-
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from taggit.models import TaggedItemBase
-
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, PageChooserPanel, MultiFieldPanel
-from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import (
+    FieldPanel, InlinePanel, PageChooserPanel, MultiFieldPanel
+)
 from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
-
 from wagtailautocomplete.edit_handlers import AutocompletePanel
 
 
@@ -42,6 +39,7 @@ class MagazineIndexPage(Page):
 
         # TODO: see if there is a better way to deal with
         # irregular month lengths for archive threshold
+        # pylint: disable=E501
         archive_threshold = datetime.date.today() - timedelta(days=archive_days_ago)
 
         published_issues = MagazineIssue.objects.live().order_by('-publication_date')
@@ -111,7 +109,7 @@ class MagazineIssue(Page):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request)
-
+        # pylint: disable=E501
         context['articles_by_department'] = MagazineArticle.objects.child_of(self).live().order_by('department__name')
 
         return context
@@ -222,4 +220,3 @@ class MagazineIssueFeaturedArticle(Orderable):
     panels = [
         PageChooserPanel('article')
     ]
-
