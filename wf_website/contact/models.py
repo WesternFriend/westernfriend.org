cd845f6acd0ace9307d7ebf2f28081a52f599fe3
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-18 23:22:14 +0200

    Add ContactInexPage

--- wf_website/contact/models.py
+++ wf_website/contact/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class ContactIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
+
+    subpage_types = ["Contact"]
+
+    max_count = 1
 
 
 class Contact(Page):
     class Meta:
         db_table = "contact"
+
+    parent_page_types = ["ContactIndexPage"]
+    subpage_types = []
