534a695221c68b1e5bd9f60ad3b940ab72f5f494
Brylie Christopher Oxley <brylie@amble.fi>
2019-03-18 23:58:25 +0200

    Add Contact detail view

--- wf_website/contact/views.py
+++ wf_website/contact/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Contact
+
+
+class ContactDetail(DetailView):
+    model = Contact
