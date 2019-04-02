3c1c4018a3e84bcdd354fa114bd22453862043b0
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-02 21:02:55 +0300

    Cleanup

--- wf_website/author/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthorConfig(AppConfig):
-    name = 'author'
--- wf_website/author/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AuthorDetail
-
-urlpatterns = [
-    path('<slug:slug>/', AuthorDetail.as_view(), name='author-detail')
-]
