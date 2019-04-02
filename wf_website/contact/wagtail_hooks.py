5cee92012ce984a0ccf8294e8aa9f5b4dd25b782
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-02 21:49:20 +0300

    Register ContactType with Wagtail admin

--- wf_website/contact/wagtail_hooks.py
+++ wf_website/contact/wagtail_hooks.py
@@ -1,9 +1,25 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
-from .models import Contact
+from .models import Contact, ContactType
 
 
 class ContactModelAdmin(ModelAdmin):
+    model = ContactType
+    menu_icon = "mail"
+    menu_label = "Contact Types"
+    menu_order = 300
+    add_to_settings_menu = True
+    exclude_from_explorer = False
+    list_per_page = 10
+
+    # list_display = ("given_name", "family_name", "full_name", "slug")
+    # search_fields = (
+    #     'given_name',
+    #     'family_name',
+    # )
+
+
+class ContactTypeModelAdmin(ModelAdmin):
     model = Contact
     menu_icon = "mail"
     menu_label = "Contacts"
@@ -24,3 +40,4 @@ class ContactModelAdmin(ModelAdmin):
 
 
 modeladmin_register(ContactModelAdmin)
+modeladmin_register(ContactTypeModelAdmin)
