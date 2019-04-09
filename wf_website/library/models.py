2adad825049696876450dbc0acd8c00d3feb9213
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-09 22:08:57 +0300

    Initial LibraryItem model

--- wf_website/library/models.py
+++ wf_website/library/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
+
+from modelcluster.fields import ParentalManyToManyField
+
+
+class LibraryItem(Page):
+    authored_by = ParentalManyToManyField("contact.Contact", related_name="media_items")
+    publication_date = models.DateField("Publication date")
+    body = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock()),
+            ("image", ImageChooserBlock()),
+            ("document", DocumentChooserBlock()),
+            ("embed", EmbedBlock()),
+            ("url", blocks.URLBlock()),
+            ("quote", blocks.BlockQuoteBlock()),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("authored_by"),
+        FieldPanel("publication_date"),
+        StreamFieldPanel("body"),
+    ]
