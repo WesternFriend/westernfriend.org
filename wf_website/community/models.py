b7ac4128b1e225eb7750502aeaed40d310dcd0e9
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-19 23:41:33 +0300

    Initial CommunityPage model

--- wf_website/community/models.py
+++ wf_website/community/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page, Orderable
 
-# Create your models here.
+
+class CommunityPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+        # InlinePanel(
+        #     "yearly_meetings",
+        #     heading="Yearly Meetings",
+        #     # pylint: disable=E501
+        #     help_text="Select yearly meetings for the community directory",
+        # ),
+    ]
+
+    subpage_types = []
+
+    max_count = 1
+
+    # def get_context(self, request, *args, **kwargs):
+    #     context = super().get_context(request)
+    #     # pylint: disable=E501
+    #     # TODO: get upcoming events
+    #     context['upcoming_events'] = MagazineIssue.objects.live().order_by('-publication_date').first()
+
+    #     return context
