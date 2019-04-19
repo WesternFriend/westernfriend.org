86411ae3669f6122c68a534ad9a8e2061b7718b6
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-20 00:09:30 +0300

    Add YearlyMeetingPage and relationship to CommunityPage

--- wf_website/community/models.py
+++ wf_website/community/models.py
@@ -1,6 +1,11 @@
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, PageChooserPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page, Orderable
+
+from modelcluster.fields import ParentalKey
+from wagtailautocomplete.edit_handlers import AutocompletePanel
 
 
 class CommunityPage(Page):
@@ -8,15 +13,15 @@ class CommunityPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("intro", classname="full"),
-        # InlinePanel(
-        #     "yearly_meetings",
-        #     heading="Yearly Meetings",
-        #     # pylint: disable=E501
-        #     help_text="Select yearly meetings for the community directory",
-        # ),
+        InlinePanel(
+            "yearly_meetings",
+            heading="Yearly Meetings",
+            # pylint: disable=E501
+            help_text="Select yearly meetings for the community directory",
+        ),
     ]
 
-    subpage_types = []
+    subpage_types = ["YearlyMeetingPage"]
 
     max_count = 1
 
@@ -27,3 +32,45 @@ class CommunityPage(Page):
     #     context['upcoming_events'] = MagazineIssue.objects.live().order_by('-publication_date').first()
 
     #     return context
+
+
+class YearlyMeetingPage(Page):
+    intro = RichTextField(blank=True)
+
+    yearly_meeting = models.ForeignKey(
+        "contact.Contact",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+        AutocompletePanel("yearly_meeting", page_type="contact.Contact"),
+    ]
+
+    subpage_types = []
+
+
+class CommunityPageYearlyMeeting(Orderable):
+    community_page_instance = ParentalKey(
+        "community.CommunityPage",
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="yearly_meetings",
+    )
+
+    yearly_meeting = models.ForeignKey(
+        YearlyMeetingPage, null=True, on_delete=models.CASCADE, related_name="+"
+    )
+
+    panels = [FieldPanel("yearly_meeting")]
+
+    @property
+    def title(self):
+        return self.yearly_meeting.title
+
+    @property
+    def intro(self):
+        return self.article.intro
