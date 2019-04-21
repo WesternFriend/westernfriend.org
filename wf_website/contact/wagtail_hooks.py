82cd4c64ce6d27b7da2d3363c079f73c32415e01
Brylie Christopher Oxley <brylie@amble.fi>
2019-04-22 01:53:51 +0300

    Remove ContactTypeModelAdmin

--- wf_website/contact/wagtail_hooks.py
+++ wf_website/contact/wagtail_hooks.py
@@ -1,16 +1,6 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
-from .models import Contact, ContactType
-
-
-class ContactTypeModelAdmin(ModelAdmin):
-    model = ContactType
-    menu_icon = "mail"
-    menu_label = "Contact Types"
-    menu_order = 300
-    add_to_settings_menu = True
-    exclude_from_explorer = False
-    list_per_page = 10
+from .models import Contact
 
 
 class ContactModelAdmin(ModelAdmin):
@@ -29,4 +19,3 @@ class ContactModelAdmin(ModelAdmin):
 
 
 modeladmin_register(ContactModelAdmin)
-modeladmin_register(ContactTypeModelAdmin)
