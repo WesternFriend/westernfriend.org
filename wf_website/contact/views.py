34a5153e6033e35420016fd2e5267dacf93ea90c
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-05 23:58:07 +0300

    Cleanup

--- wf_website/contact/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import DetailView
-
-from .models import Contact
-
-
-class ContactDetail(DetailView):
-    model = Contact
--- wf_website/wf_website/urls.py
+++ wf_website/wf_website/urls.py
@@ -8,7 +8,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
-from contact import urls as contact_urls
+# from contact import urls as contact_urls
 from magazine import urls as magazine_urls
 from search import views as search_views
 
@@ -18,7 +18,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     url(r"^admin/autocomplete/", include(autocomplete_admin_urls)),
     url(r"^admin/", include(wagtailadmin_urls)),
-    path("contacts/", include(contact_urls), name="contacts"),
     path("magazine/", include(magazine_urls), name="magazine"),
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"^search/$", search_views.search, name="search"),
@@ -39,4 +38,5 @@ if settings.DEBUG:
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
