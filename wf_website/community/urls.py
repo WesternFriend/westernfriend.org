038751616b8fedff5e2417917af5bf1fd3dab47b
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-09 20:56:35 +0300

    Name quaker organizations route

--- wf_website/community/urls.py
+++ wf_website/community/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from community.views import QuakerOrganizationList
 
 urlpatterns = [
-    path("quaker-organizations/", QuakerOrganizationList.as_view()),
+    path(
+        "quaker-organizations/",
+        QuakerOrganizationList.as_view(),
+        name="quaker_organizations"
+    ),
 ]
