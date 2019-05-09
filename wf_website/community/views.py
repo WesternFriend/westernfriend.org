3d626c268485d8c201f99f2166953985f66b605a
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-09 20:29:30 +0300

    Add Quaker Organizations view and route

--- wf_website/community/views.py
+++ wf_website/community/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from contact.models import Contact
 
-# Create your views here.
+
+class QuakerOrganizationList(ListView):
+    model = Contact
+    context_object_name = "organizations"
+    template_name = "community/quaker_organization_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        return queryset.filter(contact_type="quaker_organization")
