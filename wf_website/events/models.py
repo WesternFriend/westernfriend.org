782d408bb912dd8b49917cbc0e67117c8e70462c
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-10 23:44:25 +0300

    Initial working pagination

--- wf_website/events/models.py
+++ wf_website/events/models.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
 from django.db.models import Q
 
@@ -48,7 +49,23 @@ class EventsIndexPage(Page):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request)
 
-        context["events"] = Event.objects.all().filter(
+        upcoming_events = Event.objects.all().filter(
             Q(date__gt=date.today())).order_by('date')
+
+        # Show three archive issues per page
+        paginator = Paginator(upcoming_events, 3)
+
+        upcoming_events_page = request.GET.get("page")
+
+        try:
+            paginated_events = paginator.page(upcoming_events_page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            paginated_events = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            paginated_events = paginator.page(paginator.num_pages)
+
+        context["events"] = paginated_events
 
         return context
