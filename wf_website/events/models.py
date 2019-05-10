d36dd2ef3598cb626e6e948f61d8cc52c64311a0
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-10 22:16:23 +0300

    Initial event model

--- wf_website/events/models.py
+++ wf_website/events/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
 # Create your models here.
+
+
+class event(Page):
+    description = RichTextField(blank=True)
+
+    date = models.DateTimeField()
+
+    website = models.URLField(blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        FieldPanel("date"),
+        FieldPanel("website")
+    ]
+
+    class Meta:
+        db_table = "events"
