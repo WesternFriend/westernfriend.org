8feb84c8e25a8e9f4c5427449712aec84a1f1841
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-12 17:19:29 +0300

    Add routable page app

--- wf_website/wf_website/settings/base.py
+++ wf_website/wf_website/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "wagtail.contrib.forms",
     "wagtail.contrib.modeladmin",
     "wagtail.contrib.redirects",
+    "wagtail.contrib.routable_page",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
