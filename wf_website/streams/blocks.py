eecacb240aaafbce11e7163ca767ea322f21a6ef
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-18 23:50:48 +0300

    Add image align choice block

--- wf_website/streams/blocks.py
+++ wf_website/streams/blocks.py
@@ -1,12 +1,23 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
+IMAGE_ALIGN_CHOICES = [
+    ("left", "Left"),
+    ("right", "Right"),
+]
+
 
 class CardBlock(blocks.StructBlock):
     """Card with title, text, and image."""
     title = blocks.CharBlock(required=True, help_text="Add a title")
     text = blocks.RichTextBlock(required=False)
     image = ImageChooserBlock(required=False)
+    image_align = blocks.ChoiceBlock(
+        required=False,
+        choices=IMAGE_ALIGN_CHOICES,
+        default="left",
+        help_text="Whether to align the image left or right on the block."
+    )
     button_text = blocks.CharBlock(required=False)
     page_link = blocks.PageChooserBlock(requried=False)
 
