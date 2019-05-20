d096666d8be9ca22fb42d070379ffa999b67bfd1
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-21 00:10:37 +0300

    Add ContactsGroup and sub ModelAdmins

--- wf_website/contact/wagtail_hooks.py
+++ wf_website/contact/wagtail_hooks.py
@@ -1,34 +1,64 @@
 from django.utils.html import format_html_join
 from django.conf import settings
 
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register)
+
 from wagtail.core import hooks
 
-from .models import Contact
+from .models import Person, Meeting, Organization
 
 
-class ContactModelAdmin(ModelAdmin):
-    model = Contact
-    menu_icon = "mail"
-    menu_label = "Contacts"
-    menu_order = 200
-    add_to_settings_menu = False
-    exclude_from_explorer = False
+class PersonModelAdmin(ModelAdmin):
+    model = Person
+    menu_icon = "user"
+    menu_label = "People"
     list_per_page = 10
     ordering = ["family_name", "given_name"]
-    list_display = ("title", "contact_type")
+    list_display = ("title",)
     empty_value_display = "-"
     search_fields = ("given_name", "family_name")
-    list_filter = ("contact_type",)
 
 
-modeladmin_register(ContactModelAdmin)
+class MeetingModelAdmin(ModelAdmin):
+    model = Meeting
+    menu_icon = "group"
+    menu_label = "Meetings"
+    list_per_page = 10
+    ordering = ["title"]
+    list_display = ("title", "meeting_type")
+    empty_value_display = "-"
+    search_fields = ("title")
+    list_filter = ("meeting_type",)
+
+
+class OrganizationModelAdmin(ModelAdmin):
+    model = Organization
+    menu_icon = "home"
+    menu_label = "Organizations"
+    list_per_page = 10
+    ordering = ["title"]
+    list_display = ("title",)
+    empty_value_display = "-"
+    search_fields = ("title")
+
+
+class ContactsGroup(ModelAdminGroup):
+    menu_label = "Contacts"
+    menu_icon = "mail"
+    menu_order = 200
+    items = (PersonModelAdmin, MeetingModelAdmin, OrganizationModelAdmin)
+
+
+modeladmin_register(ContactsGroup)
 
 
 @hooks.register('insert_editor_js')
 def editor_js():
     js_files = [
-        'contact/js/contact_slug.js',
+        'contact/js/contact_person_slug.js',
     ]
     js_includes = format_html_join('\n', '<script src="{0}{1}"></script>',
                                    ((settings.STATIC_URL, filename)
