ff7a4fcf8b0e77ea857b18025a7543e66bb9b68b
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-21 00:15:23 +0300

    Fix Quaker Organizations view

--- wf_website/community/views.py
+++ wf_website/community/views.py
@@ -1,13 +1,8 @@
 from django.views.generic import ListView
-from contact.models import Contact
+from contact.models import Organization
 
 
 class QuakerOrganizationList(ListView):
-    model = Contact
+    model = Organization
     context_object_name = "organizations"
     template_name = "community/quaker_organization_list.html"
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        return queryset.filter(contact_type="quaker_organization")
