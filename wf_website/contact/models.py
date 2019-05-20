25dcf88b4f739abc1371830af8601ef2d4aecfc8
Brylie Christopher Oxley <brylie@amble.fi>
2019-05-21 00:09:41 +0300

    Refactor contact model into Person, Meeting, and Organziation

--- wf_website/contact/models.py
+++ wf_website/contact/models.py
@@ -11,73 +11,102 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.search import index
 
-CONTACT_TYPE_CHOICES = (
+MEETING_TYPE_CHOICES = (
     ("monthly_meeting", "Monthly Meeting"),
-    ("person", "Person"),
-    ("quaker_organization", "Quaker Organization"),
     ("quarterly_meeting", "Quarterly Meeting"),
     ("worship_group", "Worship Group"),
     ("yearly_meeting", "Yearly Meeting"),
 )
 
 
-class Contact(Page):
+class Person(Page):
     given_name = models.CharField(
         max_length=255,
         default="",
-        help_text="Enter the given name for a person. This can also be used for an organization name.",
+        help_text="Enter the given name for a person.",
     )
 
     family_name = models.CharField(max_length=255, blank=True, default="")
 
-    contact_type = models.CharField(
-        max_length=255, choices=CONTACT_TYPE_CHOICES)
-
-    description = models.CharField(max_length=255, blank=True, null=True)
-
-    full_name = models.CharField(max_length=255, editable=False, null=True)
-
-    website = models.URLField(null=True, blank=True)
-
     content_panels = [
         FieldPanel("given_name"),
         FieldPanel("family_name"),
-        FieldPanel("description"),
-        FieldPanel("website"),
-        FieldPanel("contact_type"),
     ]
 
     class Meta:
-        db_table = "contact"
+        db_table = "person"
         ordering = ["title"]
 
     def save(self, *args, **kwargs):
         full_name = f"{self.given_name} {self.family_name}"
         self.title = full_name.strip()
 
-        super(Contact, self).save(*args, **kwargs)
-
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request)
-        # pylint: disable=E501
-        # TODO: get upcoming events
-        # context['upcoming_events'] = MagazineIssue.objects.live().order_by('-publication_date').first()
-
-        context["quarterly_meetings"] = Contact.objects.child_of(
-            self).filter(contact_type="quarterly_meeting")
-
-        context["monthly_meetings"] = Contact.objects.descendant_of(
-            self).filter(contact_type="monthly_meeting")
-
-        context["worship_groups"] = Contact.objects.descendant_of(
-            self).filter(contact_type="worship_group")
-
-        return context
+        super(Person, self).save(*args, **kwargs)
 
     search_fields = [
         index.SearchField("given_name", partial_match=True),
         index.SearchField("family_name", partial_match=True),
     ]
 
-    parent_page_types = ["community.CommunityPage", "Contact"]
-    subpage_types = ["Contact"]
+    parent_page_types = ["community.CommunityPage"]
+    subpage_types = []
+
+
+class Meeting(Page):
+    meeting_type = models.CharField(
+        max_length=255, choices=MEETING_TYPE_CHOICES)
+
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    website = models.URLField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        FieldPanel("website"),
+        FieldPanel("meeting_type"),
+    ]
+
+    parent_page_types = ["community.CommunityPage", "Meeting"]
+    subpage_types = ["Meeting"]
+
+    class Meta:
+        db_table = "meeting"
+        ordering = ["title"]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+
+        context["quarterly_meetings"] = Meeting.objects.child_of(
+            self).filter(contact_type="quarterly_meeting")
+
+        context["monthly_meetings"] = Meeting.objects.descendant_of(
+            self).filter(contact_type="monthly_meeting")
+
+        context["worship_groups"] = Meeting.objects.descendant_of(
+            self).filter(contact_type="worship_group")
+
+        return context
+
+
+class Organization(Page):
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    website = models.URLField(null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        FieldPanel("website"),
+    ]
+
+    parent_page_types = ["community.CommunityPage"]
+    subpage_types = []
+
+    class Meta:
+        db_table = "organization"
+        ordering = ["title"]
+
+
+class Contact(Page):
+    # TODO: determine how to delete this model
+    class Meta:
+        db_table = "contact"
