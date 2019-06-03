34f9e1b2e53eef58de96c1f809c8fec95a323080
Brylie Christopher Oxley <brylie@amble.fi>
2019-06-03 23:12:34 +0300

    Add facet fields

--- library/models.py
+++ library/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import (
     FieldPanel,
     InlinePanel,
+    MultiFieldPanel,
     PageChooserPanel,
     StreamFieldPanel,
 )
@@ -17,6 +18,69 @@ from modelcluster.fields import (
     ParentalKey,
 )
 
+AUDIENCE_CHOICES = (
+    ("hs_and_older", "HS and older"),
+    ("intergenerational", "Intergenerational"),
+    ("grades_k-5", "Grades K-5"),
+    ("grades_6-10", "Grades 6-10"),
+)
+
+GENRE_CHOICES = (
+    ("biography", "Biography"),
+    ("commentary", "Commentary"),
+    ("documentary", "Documentary"),
+    ("epistle", "Epistle"),    
+    ("exposition", "Exposition"),
+    ("fiction", "Fiction"),
+    ("history", "History"),
+    ("humor", "Humor"),
+    ("keynote_talk", "Keynote Talk"),
+    ("lesson_plan", "Lesson Plan"),
+    ("memorial_minute", "Memorial Minute"),
+    ("narrative", "Narrative"),
+    ("poetry", "Poetry"),
+    ("prayer", "Prayer"),
+    ("reference", "Reference"),   
+)
+
+MEDIUM_CHOICES = (
+    ("audio", "Audio production"),
+    ("blog", "Blog"),
+    ("drawing_painting", "Drawing/Painting"),
+    ("photograph", "Photograph"),
+    ("print", "Print document"),
+    ("video", "Video production"),
+    ("website", "Website"),
+)
+
+TIME_PERIOD_CHOICES = (
+    ("timeless", "Timeless"),
+    ("1400s", "1400s"),
+    ("1500s", "1500s"),
+    ("1600s", "1600s"),
+    ("1700s", "1700s"),
+    ("1800s", "1800s"),
+    ("1900s", "1900s"),
+    ("2000s", "2000s"),
+)
+
+TOPIC_CHOICES = (
+    ("community", "Community"),
+    ("equality", "Equality"),
+    ("good_order_in_quaker_meetings", "Good Order in Quaker Meetings"),
+    ("integrity", "Integrity"),
+    ("the_light", "The Light"),
+    ("peace", "Peace"),
+    ("simplicity", "Simplicity"),
+    ("stewardship", "Stewardship"),
+    ("quaker_camps", "Quaker Camps"),
+    ("quaker_culture", "Quaker Culture"),
+    ("quaker_public_policy_organizations", "Quaker Public Policy Organizations"),
+    ("quaker_publishers", "Quaker Publishers"),
+    ("quaker_retreat_centers", "Quaker Retreat Centers"),
+    ("quaker_schools", "Quaker Schools"),
+    ("quaker_service_organizations", "Quaker Service Organizations"),
+)
 
 class LibraryItem(Page):
     publication_date = models.DateField("Publication date")
@@ -30,6 +94,16 @@ class LibraryItem(Page):
             ("quote", blocks.BlockQuoteBlock()),
         ]
     )
+    audience = models.CharField(
+        max_length=255, choices=AUDIENCE_CHOICES, null=True)
+    genre = models.CharField(
+        max_length=255, choices=GENRE_CHOICES, null=True)
+    medium = models.CharField(
+        max_length=255, choices=MEDIUM_CHOICES, null=True)
+    time_period = models.CharField(
+        max_length=255, choices=TIME_PERIOD_CHOICES, null=True)
+    topic = models.CharField(
+        max_length=255, choices=TOPIC_CHOICES, null=True)
 
     content_panels = Page.content_panels + [
         InlinePanel(
@@ -39,6 +113,16 @@ class LibraryItem(Page):
         ),
         FieldPanel("publication_date"),
         StreamFieldPanel("body"),
+        MultiFieldPanel(
+            children=[
+                FieldPanel("audience"),
+                FieldPanel("genre"),
+                FieldPanel("medium"),
+                FieldPanel("time_period"),
+                FieldPanel("topic"),
+            ],
+            heading="Categorization",
+        )
     ]
 
     parent_page_types = ["LibraryIndexPage"]
