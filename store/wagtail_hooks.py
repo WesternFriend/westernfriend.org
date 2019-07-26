0f85babc11b5d87e4ee07dc7f8cbc3f8c98fa754
Brylie Christopher Oxley <brylie@amble.fi>
2019-07-27 00:50:59 +0300

    Remove product categories

--- store/wagtail_hooks.py
+++ store/wagtail_hooks.py
@@ -4,15 +4,7 @@ from wagtail.contrib.modeladmin.options import (
     modeladmin_register
 )
 
-from store.models import Category, Product
-
-
-class CategoryModelAdmin(ModelAdmin):
-    model = Category
-    menu_icon = "fa-clone"
-    menu_label = "Categories"
-    list_per_page = 10
-    list_display = ("title",)
+from store.models import Product
 
 
 class ProductModelAdmin(ModelAdmin):
@@ -28,7 +20,6 @@ class StoreGroup(ModelAdminGroup):
     menu_icon = "fa-money"
     menu_order = 300
     items = (
-        CategoryModelAdmin,
         ProductModelAdmin,
     )
 
