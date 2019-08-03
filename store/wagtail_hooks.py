a95a323ef4754d390b4f217b29807fad2ea77dab
Brylie Christopher Oxley <brylie@amble.fi>
2019-08-03 05:27:05 +0300

    Initial OrderModelAdmin

--- store/wagtail_hooks.py
+++ store/wagtail_hooks.py
@@ -5,9 +5,10 @@ from wagtail.contrib.modeladmin.options import (
 )
 
 from store.models import Book
+from orders.models import Order
 
 
-class ProductModelAdmin(ModelAdmin):
+class BookModelAdmin(ModelAdmin):
     model = Book
     menu_icon = "fa-book"
     menu_label = "Books"
@@ -15,12 +16,27 @@ class ProductModelAdmin(ModelAdmin):
     list_display = ("title",)
 
 
+class OrderModelAdmin(ModelAdmin):
+    """Order admin."""
+
+    model = Order
+    menu_label = "Orders"
+    menu_icon = "placeholder"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("email", "full_name", "paid",)
+    search_fields = ("email", "full_name",)
+    list_filter = ("paid",)
+
+
 class StoreGroup(ModelAdminGroup):
     menu_label = "Store"
     menu_icon = "fa-money"
     menu_order = 300
     items = (
-        ProductModelAdmin,
+        BookModelAdmin,
+        OrderModelAdmin,
     )
 
 
