493efc18e3e2ffea2318c8c98cf2421ecb611966
Brylie Christopher Oxley <brylie@amble.fi>
2019-08-27 20:58:51 +0300

    Add to cart form minimum 1 item

--- cart/forms.py
+++ cart/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(
+        min_value=1,
+    )
 
     update = forms.BooleanField(
         required=False,
