199548268764ba7e6bc9efd398a04a5943ae2d3c
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-08 21:23:33 +0300

    Add braintree_id field to Order

--- orders/models.py
+++ orders/models.py
@@ -6,17 +6,13 @@ from wagtail.core.fields import StreamField
 from wagtail.core.models import Orderable, Page
 from wagtail.snippets.models import register_snippet
 
-from modelcluster.fields import (
-    ParentalKey,
-)
+from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 
 
 class Order(ClusterableModel):
     given_name = models.CharField(
-        max_length=255,
-        default="",
-        help_text="Enter the given name for the recipient.",
+        max_length=255, default="", help_text="Enter the given name for the recipient."
     )
     family_name = models.CharField(
         max_length=255,
@@ -25,38 +21,32 @@ class Order(ClusterableModel):
         help_text="Enter the family name for the recipient.",
     )
     email = models.EmailField(
-        help_text="Provide an email, so we can communicate any issues regarding this order.",
+        help_text="Provide an email, so we can communicate any issues regarding this order."
     )
     street_address = models.CharField(
         max_length=255,
         blank=True,
         default="",
-        help_text="The street address where this order should be shipped."
+        help_text="The street address where this order should be shipped.",
     )
     postal_code = models.CharField(
-        max_length=16,
-        help_text="Postal code for the shipping address."
+        max_length=16, help_text="Postal code for the shipping address."
     )
     po_box_number = models.CharField(
-        max_length=32,
-        blank=True,
-        default="",
-        help_text="P.O. Box, if relevant.",
+        max_length=32, blank=True, default="", help_text="P.O. Box, if relevant."
     )
     address_locality = models.CharField(
-        max_length=255,
-        help_text="City for the shipping address.",
+        max_length=255, help_text="City for the shipping address."
     )
     address_region = models.CharField(
-        max_length=255,
-        help_text="State for the shipping address."
+        max_length=255, help_text="State for the shipping address."
     )
     address_country = models.CharField(
-        max_length=255,
-        default="United States",
-        help_text="Country for shipping.",
+        max_length=255, default="United States", help_text="Country for shipping."
     )
     paid = models.BooleanField(default=False)
+
+    braintree_id = models.CharField(max_length=255, blank=True)
 
     panels = [
         FieldPanel("given_name"),
@@ -85,20 +75,13 @@ class Order(ClusterableModel):
 
 class OrderItem(Orderable):
     order = ParentalKey(
-        Order,
-        related_name="items",
-        on_delete=models.CASCADE,
-        blank=False,
+        Order, related_name="items", on_delete=models.CASCADE, blank=False
     )
     product = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
-    panels = [
-        FieldPanel("product"),
-        FieldPanel("price"),
-        FieldPanel("quantity"),
-    ]
+    panels = [FieldPanel("product"), FieldPanel("price"), FieldPanel("quantity")]
 
     def __str__(self):
         return f"{self.quantity}x {self.product} @ { self.price}"
