a76bb30b3010e0df4ac529599885405e8544b4a7
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-08 22:47:49 +0300

    Include payment URLs

--- wf_website/urls.py
+++ wf_website/urls.py
@@ -17,9 +17,10 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     url(r"^admin/autocomplete/", include(autocomplete_admin_urls)),
     url(r"^admin/", include(wagtailadmin_urls)),
-    path('cart/', include('cart.urls', namespace='cart')),
+    path("cart/", include("cart.urls", namespace="cart")),
     path("magazine/", include(magazine_urls), name="magazine"),
     path("orders/", include("orders.urls", namespace="orders")),
+    path("payment/", include("payment.urls", namespace="payment")),
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"^search/$", search_views.search, name="search"),
     # For anything not caught by a more specific rule above, hand over to
@@ -39,5 +40,4 @@ if settings.DEBUG:
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
