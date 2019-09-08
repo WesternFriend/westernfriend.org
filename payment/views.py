6d99c1df623c9e5a5b3fe25274d1c4136e488b8a
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-08 22:48:22 +0300

    Initial payment views

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+import braintree
+
+from orders.models import Order
+
+
+def payment_process(request):
+    order_id = request.session.get("order_id")
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == "POST":
+        # retrieve payment nonce
+        nonce = request.POST.get("payment_method_nonce", None)
+
+        # create and submit transaction
+        result = braintree.Transaction.sale(
+            {
+                "amount": order.get_total_cost(),
+                "payment_method_nonce": nonce,
+                "options": {"submit_for_settlement": True},
+            }
+        )
+
+        if result.is_success:
+            # mark order as paid
+            order.paid = True
+
+            # store Braintree transaction ID
+            order.braintree_id = result.transaction.id
+
+            order.save()
+
+            return redirect("payment:done")
+        else:
+            return redirect("payment:canceled")
+    else:
+        client_token = braintree.ClientToken.generate()
+
+        return render(
+            request,
+            "payment/process.html",
+            {"order": order, "client_token": client_token},
+        )
+
+
+def payment_done(request):
+    return render(request, "payment/done.html")
+
+
+def payment_canceled(request):
+    return render(request, "payment/canceled.html")
