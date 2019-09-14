d58fd5b27b2c6e2976182e25712f12dbe602da84
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-14 21:21:22 +0300

    Initial book shipping cost calculator

---
+++ shipping/calculator.py
@@ -0,0 +1,13 @@
+def get_book_shipping_cost(products):
+    """
+    Calculate shipping costs for books in a cart/order.
+
+    The shipping rules are flat rate for each book,
+    with discounts offered for ordering more books.
+
+    The product set can contain other product types,
+    so this function should only consider the quantity of books.
+    """
+
+    # returning a dummy value for now
+    return 10
