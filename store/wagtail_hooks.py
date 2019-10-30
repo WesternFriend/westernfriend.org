657381f896c71b1e140dbc7bfac3f37a752bb0fc
Brylie Christopher Oxley <brylie@amble.fi>
2019-10-30 19:18:26 +0200

    Lint

--- store/wagtail_hooks.py
+++ store/wagtail_hooks.py
@@ -26,10 +26,20 @@ class OrderModelAdmin(ModelAdmin):
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("purchaser_email", "purchaser_full_name", "paid",)
-    search_fields = ("purchaser_email", "purchaser_given_name", "purchaser_family_name", "purchaser_meeting_or_organization", "recipient_name",)
+    search_fields = (
+        "purchaser_email",
+        "purchaser_given_name", 
+        "purchaser_family_name", 
+        "purchaser_meeting_or_organization", 
+        "recipient_name",)
     list_filter = ("paid",)
     inspect_view_enabled = True
-    inspect_view_fields = ["id", "purchaser_given_name", "purchaser_family_name", "purchaser_meeting_or_organization", "recipient_name", "items", ]
+    inspect_view_fields = [
+        "id", 
+        "purchaser_given_name", 
+        "purchaser_family_name", 
+        "purchaser_meeting_or_organization", 
+        "recipient_name", "items", ]
     inspect_template_name = "store/inspect_order.html"
 
 
