89871cd57c69da181dbc02862fb5cea14c8249cc
Brylie Christopher Oxley <brylie@amble.fi>
2019-12-07 23:45:35 +0200

    Add registration form

--- subscription/forms.py
+++ subscription/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Subscription
 
 
@@ -25,3 +28,9 @@ class SubscriptionCreateForm(forms.ModelForm):
             "subscriber_address_region": "State",
             "subscriber_address_country": "Country",
         }
+
+
+class UserRegisterationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
