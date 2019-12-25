f8c5e67bef91964f9fa6ab69205b281e91262383
Brylie Christopher Oxley <brylie@amble.fi>
2019-12-25 19:51:54 +0200

    Initial memorial index page

--- memorials/models.py
+++ memorials/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+#from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+
+class MemorialIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    max_count = 1
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro")
+    ]
