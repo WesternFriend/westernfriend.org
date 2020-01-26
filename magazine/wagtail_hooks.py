487cb8ed54be0a6b031b4230788905053ed4fb4e
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-26 20:53:56 +0200

    Create Magazine model admin group

--- magazine/wagtail_hooks.py
+++ magazine/wagtail_hooks.py
@@ -1,9 +1,33 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
-    modeladmin_register,
+    ModelAdminGroup,
+    modeladmin_register
 )
 
-from .models import MagazineDepartment
+from .models import MagazineDepartment, MagazineIssue, ArchiveIssue
+
+
+class MagazineIssueModelAdmin(ModelAdmin):
+    model = MagazineIssue
+    menu_icon = "fa-book"
+    menu_label = "Issues"
+    list_per_page = 10
+    ordering = ["publication_date", ]
+    list_display = ("title",)
+    list_filter = ("first_published_at", )
+    empty_value_display = "-"
+    search_fields = ("title",)
+
+
+class ArchiveIssueModelAdmin(ModelAdmin):
+    model = ArchiveIssue
+    menu_icon = "fa-newspaper-o"
+    menu_label = "Archive Issues"
+    list_per_page = 10
+    ordering = ["publication_date", ]
+    list_display = ("title",)
+    empty_value_display = "-"
+    search_fields = ("title",)
 
 
 class MagazineDepartmentModelAdmin(ModelAdmin):
@@ -22,4 +46,11 @@ class MagazineDepartmentModelAdmin(ModelAdmin):
     )
 
 
-modeladmin_register(MagazineDepartmentModelAdmin)
+class MagazineGroup(ModelAdminGroup):
+    menu_label = "Magazine"
+    menu_icon = "fa-book"
+    menu_order = 100
+    items = (MagazineIssueModelAdmin, ArchiveIssueModelAdmin, MagazineDepartmentModelAdmin)
+
+
+modeladmin_register(MagazineGroup)
