3fbc1b8374a6cd66559e77cb01f88934eca6333a
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-26 19:55:08 +0200

    Fix DATABASES definition

--- wf_website/settings/base.py
+++ wf_website/settings/base.py
@@ -135,7 +135,9 @@ WSGI_APPLICATION = "wf_website.wsgi.application"
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
 else:
     DATABASES = {
         "default": {
