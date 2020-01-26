693574b993bc95fa2329eb84c607a8ea691cb76e
Brylie Christopher Oxley <brylie@amble.fi>
2020-01-26 22:06:55 +0200

    Refactor for readability

--- memorials/models.py
+++ memorials/models.py
@@ -69,12 +69,7 @@ class MemorialIndexPage(Page):
         Memorial,
     ]
 
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request)
-
-        # Populate faceted search fields
-        context["meetings"] = Meeting.objects.all()
-
+    def get_filtered_memorials(self, request):
         # Check if any query string is available
         query = request.GET.dict()
 
@@ -86,11 +81,12 @@ class MemorialIndexPage(Page):
         facets = {f"{key}__icontains": query[key]
                   for key in query if key in allowed_keys}
 
-        memorials = Memorial.objects.all().filter(**facets)
+        return Memorial.objects.all().filter(**facets)
 
+    def get_paginated_memorials(self, filtered_memorials, request):
         items_per_page = 10
 
-        paginator = Paginator(memorials, items_per_page)
+        paginator = Paginator(filtered_memorials, items_per_page)
 
         memorials_page = request.GET.get("page")
 
@@ -103,6 +99,18 @@ class MemorialIndexPage(Page):
             # If page is out of range (e.g. 9999), deliver last page of results.
             paginated_memorials = paginator.page(paginator.num_pages)
 
+        return paginated_memorials
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+
+        filtered_memorials = self.get_filtered_memorials(request)
+
+        paginated_memorials = self.get_paginated_memorials(filtered_memorials, request)
+
         context["memorials"] = paginated_memorials
+
+        # Populate faceted search fields
+        context["meetings"] = Meeting.objects.all()
 
         return context
