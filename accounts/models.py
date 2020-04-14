668793af159227d0c571a84107e371c433742ed8
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-14 19:21:34 +0300

    New custom user model and manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
-from authtools.models import AbstractEmailUser
+#from authtools.models import AbstractEmailUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+from .managers import UserManager
 
 
-class User(AbstractEmailUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    # source: https://testdriven.io/blog/django-custom-user-model/
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
