b40fbf21c8d09f1e031770d7c4ebb2d7b0673349
Brylie Christopher Oxley <brylie@amble.fi>
2020-04-14 17:17:49 +0300

    Initial departments importer

---
+++ content_migration/management/commands/import_departments.py
@@ -0,0 +1,29 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+
+from magazine.models import MagazineDepartment, MagazineDepartmentIndexPage
+
+
+class Command(BaseCommand):
+    help = "Import Departments from Drupal site"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--file", action="store", type=str)
+
+    def handle(self, *args, **options):
+        # Get the only instance of Magazine Department Index Page
+        magazine_department_index_page = MagazineDepartmentIndexPage.objects.get()
+
+        with open(options["file"]) as import_file:
+            departments = csv.DictReader(import_file)
+
+            for department in departments:
+                import_department = MagazineDepartment(
+                    title=department["title"],
+                )
+
+                # Add department to site page hiererchy
+                magazine_department_index_page.add_child(instance=import_department)
+                magazine_department_index_page.save()
+
+        self.stdout.write("All done!")
