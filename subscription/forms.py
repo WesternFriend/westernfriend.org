6b92e03f3f763e505aebe3b6cad3aef98cafba0c
Brylie Christopher Oxley <brylie@amble.fi>
2020-06-23 20:28:09 +0300

    Get Braintree plans

--- subscription/forms.py
+++ subscription/forms.py
@@ -1,13 +1,31 @@
+import os
+
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import Subscription
 
+import braintree
+
 UserModel = get_user_model()
 
 
 class SubscriptionCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        gateway = braintree.BraintreeGateway(
+            braintree.Configuration(
+                environment=braintree.Environment.Sandbox,
+                merchant_id=os.environ.get("BRAINTREE_MERCHANT_ID"),
+                public_key=os.environ.get("BRAINTREE_PUBLIC_KEY"),
+                private_key=os.environ.get("BRAINTREE_PRIVATE_KEY"),
+            )
+        )
+        plans = gateway.plan.all()
+
+        self.plans = plans
 
     class Meta:
         model = Subscription
@@ -34,4 +52,8 @@ class SubscriptionCreateForm(forms.ModelForm):
 class UserRegisterationForm(UserCreationForm):
     class Meta:
         model = UserModel
-        fields = ('email', 'password1', 'password2', )
+        fields = (
+            "email",
+            "password1",
+            "password2",
+        )
