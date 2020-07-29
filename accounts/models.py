936e0563893c87be244ffc7517dd59dde8934067
Brylie Christopher Oxley <brylie@amble.fi>
2020-07-29 17:10:19 +0300

    Add methods to check user subscription status

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+import datetime
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
 from django.utils import timezone
 
 from .managers import UserManager
@@ -20,3 +21,22 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def get_active_subscription(self):
+        """
+        Get subscription for this user that isn't expired.
+        """
+        today = datetime.datetime.today()
+
+        return self.subscriptions.get(end_date__gte=today)
+
+    @property
+    def is_subscriber(self):
+        """
+        Check whether user has active subscription.
+        """
+
+        if self.get_active_subscription() is not None:
+            return True
+
+        return False
