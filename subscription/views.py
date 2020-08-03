e0d083148a7b2ef0a06a7b7ef425d39497b87008
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-03 19:32:06 +0300

    Parse Braintree webhook

--- subscription/views.py
+++ subscription/views.py
@@ -1,7 +1,12 @@
+import json
+import os
+
 from django.http import HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+
+import braintree
 
 from subscription.models import Subscription
 
@@ -11,5 +16,21 @@ class SubscriptionWebhookView(View):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
-    def post(self, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
+        gateway = braintree.BraintreeGateway(
+            braintree.Configuration(
+                braintree.Environment.Sandbox,
+                merchant_id=os.environ.get("BRAINTREE_MERCHANT_ID"),
+                public_key=os.environ.get("BRAINTREE_PUBLIC_KEY"),
+                private_key=os.environ.get("BRAINTREE_PRIVATE_KEY")
+            )
+        )
+
+        body = json.loads(request.body)
+
+        webhook_notification = gateway.webhook_notification.parse(
+            body["bt_signature"],
+            body["bt_payload"]
+        )
+
         return HttpResponse()
