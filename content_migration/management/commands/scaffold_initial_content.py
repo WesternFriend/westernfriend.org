0e7931dec075ef37323f4dc1db7d306a24b6058a
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-25 20:55:33 +0300

    Add deep archive index page

--- content_migration/management/commands/scaffold_initial_content.py
+++ content_migration/management/commands/scaffold_initial_content.py
@@ -2,14 +2,15 @@ from django.core.management.base import BaseCommand, CommandError
 
 from wagtail.core.models import Page
 
-from contact.models import (
-    MeetingIndexPage,
-    OrganizationIndexPage,
-    PersonIndexPage
-)
+from contact.models import MeetingIndexPage, OrganizationIndexPage, PersonIndexPage
 from home.models import HomePage
 from community.models import CommunityPage
-from magazine.models import MagazineDepartmentIndexPage, MagazineIndexPage, MagazineTagIndexPage
+from magazine.models import (
+    MagazineDepartmentIndexPage,
+    MagazineIndexPage,
+    MagazineTagIndexPage,
+    DeepArchiveIndexPage,
+)
 
 
 class Command(BaseCommand):
@@ -17,16 +18,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         root_page = Page.objects.get(id=1)
-        home_page = HomePage(
-            title="Welcome"
-        )
+        home_page = HomePage(title="Welcome")
 
         root_page.add_child(instance=home_page)
         root_page.save()
 
-        community_page = CommunityPage(
-            title="Community"
-        )
+        community_page = CommunityPage(title="Community")
         magazine_index_page = MagazineIndexPage(title="Magazine")
 
         home_page.add_child(instance=community_page)
@@ -34,24 +31,22 @@ class Command(BaseCommand):
         home_page.save()
 
         # Magazine section
-        magazine_department_index_page = MagazineDepartmentIndexPage(title="Departments")
+        magazine_department_index_page = MagazineDepartmentIndexPage(
+            title="Departments"
+        )
         magazine_tag_index_page = MagazineTagIndexPage(title="Tags")
+        deep_archive_index_page = DeepArchiveIndexPage(title="Archive")
 
         magazine_index_page.add_child(instance=magazine_department_index_page)
         magazine_index_page.add_child(instance=magazine_tag_index_page)
+        magazine_index_page.add_child(instance=deep_archive_index_page)
+
         magazine_index_page.save()
 
-        
         # Community section
-        meeting_index_page = MeetingIndexPage(
-            title="Meetings"
-        )
-        organization_index_page = OrganizationIndexPage(
-            title="Organizations"
-        )
-        person_index_page = PersonIndexPage(
-            title="People"
-        )
+        meeting_index_page = MeetingIndexPage(title="Meetings")
+        organization_index_page = OrganizationIndexPage(title="Organizations")
+        person_index_page = PersonIndexPage(title="People")
 
         community_page.add_child(instance=meeting_index_page)
         community_page.add_child(instance=organization_index_page)
