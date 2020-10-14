9485166c3d4b16b05fef64c8d15eadf66783b549
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-14 18:59:32 +0300

    Debug/refactor

--- content_migration/management/commands/import_articles.py
+++ content_migration/management/commands/import_articles.py
@@ -46,6 +46,60 @@ def clean_pullquote_tags(item: BS4_Tag) -> BS4_Tag:
 
     return item
 
+def parse_article_body_blocks(body):
+    article_body_blocks = []
+
+    try:
+        soup = BeautifulSoup(body, "html.parser")
+    except:
+        soup = False
+
+        print("Could not parse article body:", body)
+
+    # Placeholder for gathering successive items
+    rich_text_value = ""
+
+    if soup:
+        for item in soup:
+
+            item_has_value = item.string is not None
+
+            if item_has_value:
+
+                item_contains_pullquote = "pullquote" in item.string
+
+                if item_contains_pullquote:
+                    # Add current rich text value as paragraph block, if not empty
+                    if rich_text_value != "":
+                        rich_text_block = ("paragraph", RichText(rich_text_value))
+
+                        article_body_blocks.append(rich_text_block)
+
+                        # reset rich text value
+                        rich_text_value = ""
+
+                    pullquotes = extract_pullquotes(item)
+
+                    # Add Pullquote block(s) to body streamfield
+                    # so they appear above the related rich text field
+                    # i.e. near the paragraph containing the pullquote
+                    for pullquote in pullquotes:
+                        block_content = ("pullquote", pullquote)
+
+                        article_body_blocks.append(block_content)
+
+                    item = clean_pullquote_tags(item)
+
+                rich_text_value += str(item)
+
+        if rich_text_value != "":
+            # Add Paragraph Block with remaining rich text elements
+            rich_text_block = ("paragraph", RichText(rich_text_value))
+
+            article_body_blocks.append(rich_text_block)
+
+    return article_body_blocks       
+
 
 class Command(BaseCommand):
     help = "Import Articles from Drupal site while linking them to Authors, Issues, Deparments, and Keywords"
@@ -58,53 +112,12 @@ class Command(BaseCommand):
         articles = pd.read_csv(options["articles_file"])
         authors = pd.read_csv("../import_data/authors_cleaned_deduped-2020-04-12.csv")
 
-        for index, row in articles.iterrows():
-            soup = BeautifulSoup(row["Body"], "html.parser")
+        for index, row in articles.iterrows():          
 
             department = MagazineDepartment.objects.get(title=row["Department"])
 
             article_body_blocks = []
-
-            # Placeholder for gathering successive items
-            rich_text_value = ""
-
-            for item in soup:
-
-                item_has_value = item.string is not None
-
-                if item_has_value:
-
-                    item_contains_pullquote = "pullquote" in item.string
-
-                    if item_contains_pullquote:
-                        # Add current rich text value as paragraph block, if not empty
-                        if rich_text_value != "":
-                            rich_text_block = ("paragraph", RichText(rich_text_value))
-
-                            article_body_blocks.append(rich_text_block)
-
-                            # reset rich text value
-                            rich_text_value = ""
-
-                        pullquotes = extract_pullquotes(item)
-
-                        # Add Pullquote block(s) to body streamfield
-                        # so they appear above the related rich text field
-                        # i.e. near the paragraph containing the pullquote
-                        for pullquote in pullquotes:
-                            block_content = ("pullquote", pullquote)
-
-                            article_body_blocks.append(block_content)
-
-                        item = clean_pullquote_tags(item)
-
-                    rich_text_value += str(item)
-
-            if rich_text_value != "":
-                # Add Paragraph Block with remaining rich text elements
-                rich_text_block = ("paragraph", RichText(rich_text_value))
-
-                article_body_blocks.append(rich_text_block)
+            #article_body_blocks = parse_article_body_blocks(row["Body"])
 
             article = MagazineArticle(
                 title=row["title"],
@@ -127,23 +140,37 @@ class Command(BaseCommand):
             # Assign authors to article
             for author in row["Authors"].split(", "):
                 authors_mask = authors["drupal_full_name"] == author
+
+                if authors_mask.sum() == 0:
+                    print("Author not found:", author)
+                if authors_mask.sum() > 1:
+                    print("Duplicate authors found:", author)
+
                 author_data = authors[authors_mask].iloc[0].to_dict()
+                drupal_full_name = author_data["drupal_full_name"]
 
                 if author_data["organization_name"] is not np.nan:
                     author = Organization.objects.get(
-                        drupal_full_name=author_data["drupal_full_name"]
+                        drupal_full_name=drupal_full_name
                     )
                 elif author_data["meeting_name"] is not np.nan:
                     author = Meeting.objects.get(
-                        drupal_full_name=author_data["drupal_full_name"]
+                        drupal_full_name=drupal_full_name
                     )
                 else:
-                    author = Person.objects.get(
-                        drupal_full_name=author_data["drupal_full_name"]
-                    )
+                    try:
+                        author = Person.objects.get(
+                            drupal_full_name=drupal_full_name
+                        )
+                    except:
+                        print("Cannot find person named:", f'"{ drupal_full_name }"')
 
-                article_author = MagazineArticleAuthor(article=article, author=author)
-                article.authors.add(article_author)
+                try:
+                    article_author = MagazineArticleAuthor(article=article, author=author)
+                    article.authors.add(article_author)
+                except:
+                    pass
+                    #print("Could not assign author to article:", author, article)
             try:
                 for keyword in row["Keywords"].split(", "):
                     article.tags.add(keyword)
--- content_migration/management/commands/import_authors.py
+++ content_migration/management/commands/import_authors.py
@@ -10,6 +10,92 @@ from contact.models import (
     PersonIndexPage,
 )
 
+def create_meeting(author):
+    meeting_index_page = MeetingIndexPage.objects.get()
+
+    meeting_name = author["meeting_name"]
+    drupal_full_name = author["drupal_full_name"]
+
+    meeting_exists = Meeting.objects.filter(
+        drupal_full_name=drupal_full_name,
+    ).exists()
+
+    # Don't create duplicate meetings
+    if not meeting_exists:
+        try:
+            meeting = Meeting(
+                title=meeting_name,
+                drupal_full_name=drupal_full_name,
+            )
+        except:
+            print("Could not create meeting:", drupal_full_name)
+
+        meeting_index_page.add_child(instance=meeting)
+
+        meeting_index_page.save()
+
+
+def create_organization(author):
+    organization_index_page = OrganizationIndexPage.objects.get()
+
+    organization_name = author["organization_name"]
+    drupal_full_name = author["drupal_full_name"]
+
+    organization_exists = Organization.objects.filter(
+        drupal_full_name=drupal_full_name,
+    ).exists()
+
+    # Avoid duplicates
+    if not organization_exists:
+        try:
+            organization = Organization(
+                title=organization_name,
+                drupal_full_name=drupal_full_name,
+            )
+        except:
+            print("Could not create organization:", drupal_full_name)
+
+        organization_index_page.add_child(instance=organization)
+
+        organization_index_page.save()
+
+
+def create_person(author):
+    person_index_page = PersonIndexPage.objects.get()
+
+    drupal_full_name = author["drupal_full_name"]
+
+    author_name_corrected = (
+        author["corrected_family_name"] != ""
+        or author["corrected_given_name"] != ""
+    )
+
+    if author_name_corrected:
+        given_name = author["corrected_given_name"]
+        family_name = author["corrected_family_name"]
+    else:
+        given_name = author["given_name"]
+        family_name = author["family_name"]
+
+    person_exists = Person.objects.filter(
+        drupal_full_name=drupal_full_name,
+    ).exists()
+
+    # Avoid duplicates
+    if not person_exists:
+        try:
+            person = Person(
+                given_name=given_name,
+                family_name=family_name,
+                drupal_full_name=drupal_full_name
+            )
+        except:
+            print("Could not create person: ", drupal_full_name)
+
+        person_index_page.add_child(instance=person)
+
+        person_index_page.save()
+
 
 class Command(BaseCommand):
     help = "Import Authors from Drupal site"
@@ -19,11 +105,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with open(options["file"]) as import_file:
-            # Get index pages for use when saving entities
-            meeting_index_page = MeetingIndexPage.objects.get()
-            organization_index_page = OrganizationIndexPage.objects.get()
-            person_index_page = PersonIndexPage.objects.get()
-
             authors = csv.DictReader(import_file)
 
             for author in authors:
@@ -32,74 +113,24 @@ class Command(BaseCommand):
                 # - Organization
                 # - Person
                 # with the condition to check for corrections to person names
+
+                drupal_full_name = author["drupal_full_name"]
+
                 author_is_meeting = author["meeting_name"] != ""
                 author_is_organization = author["organization_name"] != ""
                 author_is_person = (
-                    author["family_name"] != ""
-                    or author["given_name"] != ""
-                    or author["corrected_family_name"] != ""
-                    or author["corrected_given_name"] != ""
+                    author_is_meeting == False 
+                    and author_is_organization == False
                 )
+                   
 
                 if author_is_meeting:
-                    meeting_exists = Meeting.objects.filter(
-                        title=author["meeting_name"],
-                    ).exists()
-
-                    # Don't create duplicate meetings
-                    if not meeting_exists:
-                        meeting = Meeting(
-                            title=author["meeting_name"],
-                            drupal_full_name=author["drupal_full_name"],
-                        )
-
-                        meeting_index_page.add_child(instance=meeting)
-
-                        meeting_index_page.save()
+                    create_meeting(author)
                 elif author_is_organization:
-                    organization_exists = Organization.objects.filter(
-                        title=author["organization_name"],
-                    ).exists()
-
-                    # Avoid duplicates
-                    if not organization_exists:
-                        organization = Organization(
-                            title=author["organization_name"],
-                            drupal_full_name=author["drupal_full_name"],
-                        )
-
-                        organization_index_page.add_child(instance=organization)
-
-                        organization_index_page.save()
+                    create_organization(author)
                 elif author_is_person:
-                    author_name_corrected = (
-                        author["corrected_family_name"] != ""
-                        or author["corrected_given_name"] != ""
-                    )
-
-                    if author_name_corrected:
-                        given_name = author["corrected_given_name"]
-                        family_name = author["corrected_family_name"]
-                    else:
-                        given_name = author["given_name"]
-                        family_name = author["family_name"]
-
-                    person_exists = Person.objects.filter(
-                        given_name=given_name, family_name=family_name,
-                    ).exists()
-
-                    # Avoid duplicates
-                    if not person_exists:
-                        person = Person(
-                            given_name=given_name,
-                            family_name=family_name,
-                            drupal_full_name=author["drupal_full_name"],
-                        )
-
-                        person_index_page.add_child(instance=person)
-
-                        person_index_page.save()
+                    create_person(author)
                 else:
-                    print("unknown")
+                    print("Unknown:", drupal_full_name)
 
         self.stdout.write("All done!")
