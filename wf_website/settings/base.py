981eddae616e364b52f7de066c30f8b4eb455550
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-14 18:59:41 +0200

    Remove deprecated middleware

--- wf_website/settings/base.py
+++ wf_website/settings/base.py
@@ -111,7 +111,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
