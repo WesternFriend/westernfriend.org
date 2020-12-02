414934b697fc7a95a2df2ff7ce9bf9faf1a06a91
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-12-02 18:24:46 +0200

    Initial DonatePage model

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+
+class DonatePage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+    ]
+
+    max_count = 1
