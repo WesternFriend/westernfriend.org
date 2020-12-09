b2045621de92efe7ce79b15c13ccf81d7c9b220a
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-12-09 18:13:54 +0200

    Add suggested donation amounts field

--- donations/models.py
+++ donations/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.blocks import IntegerBlock
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
 
 
 class DonatePage(Page):
     intro = RichTextField(blank=True)
+    suggested_donation_amounts = StreamField([
+        ("amount", IntegerBlock())
+    ], null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("intro", classname="full"),
+        StreamFieldPanel("suggested_donation_amounts")
     ]
 
     max_count = 1
