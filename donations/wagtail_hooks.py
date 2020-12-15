9805a8e8b2b384600414c028fbea4721bc1c4677
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-12-15 20:35:15 +0200

    Register Donation model with Wagtail admin

---
+++ donations/wagtail_hooks.py
@@ -0,0 +1,26 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register
+)
+
+from donations.models import Donation
+
+class DonationModelAdmin(ModelAdmin):
+    """Donation admin."""
+
+    model = Donation
+    menu_label = "Donations"
+    menu_icon = "fa-gift"
+    menu_order = 291
+    add_to_settings_menu = False
+    exclude_from_explorer = True
+    list_display = (
+        "amount", 
+    )
+    inspect_view_enabled = True
+    inspect_view_fields = [
+        "amount",
+    ]
+
+
+modeladmin_register(DonationModelAdmin)
