5ac2524a8a45cd0f6a1334879d18296a5ab3c066
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-12-29 19:17:45 +0200

    Add donor given and family names

--- donations/wagtail_hooks.py
+++ donations/wagtail_hooks.py
@@ -15,17 +15,25 @@ class DonationModelAdmin(ModelAdmin):
     add_to_settings_menu = False
     exclude_from_explorer = True
     list_display = (
+        "donor_given_name",
+        "donor_family_name",
         "donor_email",
         "amount",
         "paid",
     )
     inspect_view_enabled = True
     inspect_view_fields = [
+        "donor_given_name",
+        "donor_family_name",
         "donor_email",
         "amount",
         "paid",
     ]
-    search_fields = ("donor_email",)
+    search_fields = (
+        "donor_given_name",
+        "donor_family_name",
+        "donor_email",
+    )
 
 
 modeladmin_register(DonationModelAdmin)
