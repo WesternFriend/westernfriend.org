b64468213ee72f34cba6b4a7dad112e4568bdaa0
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-01-06 20:32:02 +0200

    Lint

--- donations/forms.py
+++ donations/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from .models import Donation
 
 # TODO: Add "email" field and make it mandatory
+
+
 class DonationForm(forms.ModelForm):
     class Meta:
         model = Donation
