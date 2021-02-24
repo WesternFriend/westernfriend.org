a5c8b1f0ebc513b8c0bf070159b202d29b95a982
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-02-24 11:40:44 +0200

    Create Home Page children

--- content_migration/management/commands/scaffold_initial_content.py
+++ content_migration/management/commands/scaffold_initial_content.py
@@ -5,12 +5,22 @@ from wagtail.core.models import Page, Site
 from contact.models import MeetingIndexPage, OrganizationIndexPage, PersonIndexPage
 from home.models import HomePage
 from community.models import CommunityPage
+from donations.models import DonatePage
+from events.models import EventsIndexPage
+from forms.models import ContactFormPage
+from library.models import LibraryIndexPage
 from magazine.models import (
     MagazineDepartmentIndexPage,
     MagazineIndexPage,
     MagazineTagIndexPage,
     DeepArchiveIndexPage,
 )
+from store.models import StoreIndexPage
+from subscription.models import (
+    SubscriptionIndexPage,
+    ManageSubscriptionPage,
+)
+
 
 
 class Command(BaseCommand):
@@ -33,11 +43,26 @@ class Command(BaseCommand):
         # Otherwise, the title "Welcome to your new Wagtail site!" might be useful
         Page.objects.get(id=2).delete()
 
+        # Create Home Page children
         community_page = CommunityPage(title="Community")
+        contact_form_page = ContactFormPage(title="Contact")
+        donate_page = DonatePage(title="Donate")
+        events_page = EventsIndexPage(title="Events")
+        library_index_page = LibraryIndexPage(title="Library")
         magazine_index_page = MagazineIndexPage(title="Magazine")
+        manage_subscription_page = ManageSubscriptionPage(title="Manage subscription")
+        store_index_page = StoreIndexPage(title="Bookstore")
+        subscription_index_page = SubscriptionIndexPage(title="Subscribe")
 
         home_page.add_child(instance=community_page)
+        home_page.add_child(instance=contact_form_page)
+        home_page.add_child(instance=donate_page)
+        home_page.add_child(instance=events_page)
+        home_page.add_child(instance=library_index_page)
         home_page.add_child(instance=magazine_index_page)
+        home_page.add_child(instance=manage_subscription_page)
+        home_page.add_child(instance=store_index_page)
+        home_page.add_child(instance=subscription_index_page)
         home_page.save()
 
         # Magazine section
