bc5128e5869e5880de047fcb1f029678f9362d5c
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-02-26 12:34:04 +0200

    Initial working importer

--- content_migration/management/commands/import_library_items.py
+++ content_migration/management/commands/import_library_items.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from tqdm import tqdm
 
-from library.models import LibraryIndexPage
+from library.models import LibraryIndexPage, LibraryItem
 
 
 class Command(BaseCommand):
@@ -21,5 +21,16 @@ class Command(BaseCommand):
             library_items_csv = csv.DictReader(import_file)
             library_items = list(library_items_csv)
 
-            for library_item in tqdm(library_items, desc="Library items", unit="row"):
-                pass
+            for library_item_dict in tqdm(library_items, desc="Library items", unit="row"):
+                library_item_exists = LibraryItem.objects.filter(drupal_node_id=library_item_dict["node_id"]).exists()
+
+                if library_item_exists:
+                    library_item = LibraryItem.objects.get(drupal_node_id=library_item_dict["node_id"])
+                else:
+                    library_item = LibraryItem(
+                        title=library_item_dict["title"]
+                    )
+
+                # Add library item to library
+                library_item_index_page.add_child(instance=library_item)
+                library_item_index_page.save()
