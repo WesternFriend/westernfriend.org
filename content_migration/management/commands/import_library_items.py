ff8882e0cee8916680a93340e94a9a3f5f3f82dd
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-02-26 12:04:00 +0200

    Initial library items iterator

--- content_migration/management/commands/import_library_items.py
+++ content_migration/management/commands/import_library_items.py
@@ -1,4 +1,10 @@
+import csv
+
 from django.core.management.base import BaseCommand, CommandError
+
+from tqdm import tqdm
+
+from library.models import LibraryIndexPage
 
 
 class Command(BaseCommand):
@@ -8,4 +14,12 @@ class Command(BaseCommand):
         parser.add_argument("--file", action="store", type=str)
 
     def handle(self, *args, **options):
-        pass
+        # Get the only instance of Magazine Department Index Page
+        library_item_index_page = LibraryIndexPage.objects.get()
+
+        with open(options["file"]) as import_file:
+            library_items_csv = csv.DictReader(import_file)
+            library_items = list(library_items_csv)
+
+            for library_item in tqdm(library_items, desc="Library items", unit="row"):
+                pass
