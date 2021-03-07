71b410eafc5f4e889cb624e420b7419f23525565
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-03-07 17:56:33 +0200

    Import library item description

--- content_migration/management/commands/import_library_items.py
+++ content_migration/management/commands/import_library_items.py
@@ -30,31 +30,41 @@ class Command(BaseCommand):
             library_items_csv = csv.DictReader(import_file)
             library_items = list(library_items_csv)
 
-            for library_item_dict in tqdm(
+            for import_library_item in tqdm(
                 library_items, desc="Library items", unit="row"
             ):
                 library_item_exists = LibraryItem.objects.filter(
-                    drupal_node_id=library_item_dict["node_id"]
+                    drupal_node_id=import_library_item["node_id"]
                 ).exists()
 
                 if library_item_exists:
                     library_item = LibraryItem.objects.get(
-                        drupal_node_id=library_item_dict["node_id"]
+                        drupal_node_id=import_library_item["node_id"]
                     )
                 else:
                     library_item = LibraryItem(
-                        title=library_item_dict["title"],
-                        drupal_node_id=library_item_dict["node_id"]
+                        title=import_library_item["title"],
+                        drupal_node_id=import_library_item["node_id"],
                     )
 
-                library_item.item_audience = Audience.objects.get(title=library_item_dict["Audience"])
-                library_item.item_genre = Genre.objects.get(title=library_item_dict["Genre"])
-                library_item.item_medium = Medium.objects.get(title=library_item_dict["Medium"])
-                library_item.item_time_period = TimePeriod.objects.get(title=library_item_dict["Time Period"])
+                library_item.title = import_library_item["title"]
+                library_item.description = import_library_item["Description"]
+                library_item.item_audience = Audience.objects.get(
+                    title=import_library_item["Audience"]
+                )
+                library_item.item_genre = Genre.objects.get(
+                    title=import_library_item["Genre"]
+                )
+                library_item.item_medium = Medium.objects.get(
+                    title=import_library_item["Medium"]
+                )
+                library_item.item_time_period = TimePeriod.objects.get(
+                    title=import_library_item["Time Period"]
+                )
 
                 if not library_item_exists:
                     # Add library item to library
                     library_item_index_page.add_child(instance=library_item)
                     library_item_index_page.save()
-                
+
                 library_item.save()
