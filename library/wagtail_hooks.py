1bc94ee6374f01c4adda73dae2383e73674dc78e
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-03-10 17:59:18 +0200

    Fix search_fields; format code

--- library/wagtail_hooks.py
+++ library/wagtail_hooks.py
@@ -1,9 +1,7 @@
-from wagtail.contrib.modeladmin.options import (
-    ModelAdmin,
-    modeladmin_register
-)
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from library.models import LibraryItem
+
 
 @modeladmin_register
 class LibraryItemModelAdmin(ModelAdmin):
@@ -11,6 +9,17 @@ class LibraryItemModelAdmin(ModelAdmin):
     menu_icon = "media"
     menu_label = "Media library"
     list_per_page = 10
-    list_display = ("title", "item_audience", "item_genre", "item_medium", "item_time_period",)
-    list_filter = ("item_audience", "item_genre", "item_medium", "item_time_period",)
-    search_fields = ("title")
+    list_display = (
+        "title",
+        "item_audience",
+        "item_genre",
+        "item_medium",
+        "item_time_period",
+    )
+    list_filter = (
+        "item_audience",
+        "item_genre",
+        "item_medium",
+        "item_time_period",
+    )
+    search_fields = ("title",)
