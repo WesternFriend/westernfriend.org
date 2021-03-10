ec81bf99a3c4b317d38ba5734cc4b9899f294005
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2021-03-10 19:30:25 +0200

    Add Vimeo and  Youtube embed handler

--- content_migration/management/commands/shared.py
+++ content_migration/management/commands/shared.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+from urllib.parse import urlparse
 
 from django.core.files import File
 from django.core.files.images import ImageFile
 
 from wagtail.documents.models import Document
+from wagtail.embeds.embeds import get_embed
 from wagtail.images.models import Image
 
 import numpy as np
@@ -14,36 +16,44 @@ def parse_media_blocks(media_urls):
     media_blocks = []
 
     for url in media_urls.split(", "):
-        response = requests.get(url)
-        content_type = response.headers["content-type"]
-        file_name = url.split("/")[-1]
-        file_bytes = BytesIO(response.content)
+        domain = urlparse(url).netloc
 
-        if content_type == "application/pdf":
-            # Create file
-            document_file = File(file_bytes, name=file_name)
-
-            document = Document(title=file_name, file=document_file,)
-
-            document.save()
-
-            document_link_block = ("document", document)
-
-            media_blocks.append(document_link_block)
-        elif content_type in ["image/jpeg", "image/png"]:
-            # create image
-            image_file = ImageFile(file_bytes, name=file_name)
-
-            image = Image(title=file_name, file=image_file,)
-
-            image.save()
-
-            image_block = ("image", image)
-
-            media_blocks.append(image_block)
+        if domain in ["vimeo.com", "www.youtube.com"]:
+            embed = get_embed(url)
+            embed_tuple = ("embed", embed)
+            media_blocks.append(embed_tuple)
         else:
-            print(url)
-            print(content_type)
-            print("-----")
+            # The default should be to fetch a PDF or image file (i.e. from westernfriend.org)
+            response = requests.get(url)
+            content_type = response.headers["content-type"]
+            file_name = url.split("/")[-1]
+            file_bytes = BytesIO(response.content)
+
+            if content_type == "application/pdf":
+                # Create file
+                document_file = File(file_bytes, name=file_name)
+
+                document = Document(title=file_name, file=document_file,)
+
+                document.save()
+
+                document_link_block = ("document", document)
+
+                media_blocks.append(document_link_block)
+            elif content_type in ["image/jpeg", "image/png"]:
+                # create image
+                image_file = ImageFile(file_bytes, name=file_name)
+
+                image = Image(title=file_name, file=image_file,)
+
+                image.save()
+
+                image_block = ("image", image)
+
+                media_blocks.append(image_block)
+            else:
+                print(url)
+                print(content_type)
+                print("-----")
 
     return media_blocks
