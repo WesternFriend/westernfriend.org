873d36dc02314d0589886729a70ac79a36cec31f
Brylie Christopher Oxley <brylie@amble.fi>
2021-04-07 18:29:46 +0300

    Initial WF Pages models

--- wf_pages/models.py
+++ wf_pages/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Page
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class WfPageCollectionIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
+
+    subpage_types = ["wf_pages.WfPageCollection"]
+    max_count = 1
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+
+        collections = WfPageCollection.objects.all()
+        context["collections"] = collections
+
+        return context
+
+
+class WfPageCollection(Page):
+    panels = [FieldPanel("title")]
+
+    parent_page_types = ["wf_pages.WfPageCollectionIndexPage"]
+    subpage_types = []
+
+
+class WfPage(Page):
+    body = StreamField(
+        [
+            ("document", DocumentChooserBlock()),
+            ("heading", blocks.CharBlock(classname="full title")),
+            ("image", ImageChooserBlock()),
+            (
+                "paragraph",
+                blocks.RichTextBlock(
+                    features=[
+                        "h2",
+                        "h3",
+                        "h4",
+                        "bold",
+                        "italic",
+                        "ol",
+                        "ul",
+                        "hr",
+                        "link",
+                        "document-link",
+                        "image",
+                        "superscript",
+                        "superscript",
+                        "strikethrough",
+                        "blockquote",
+                    ]
+                ),
+            ),
+            ("pullquote", blocks.BlockQuoteBlock()),
+        ]
+    )
+    body_migrated = models.TextField(
+        help_text="Used only for content from old Drupal website.",
+        null=True,
+        blank=True,
+    )
+    collection = models.ForeignKey(
+        WfPageCollection,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="pages",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+        FieldPanel("collection"),
+    ]
+
+    class Meta:
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
