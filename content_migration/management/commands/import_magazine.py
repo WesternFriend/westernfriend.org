065a4e351e33104c6c028896f6714e0be7d63fea
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-17 10:28:48 +0300

    Use magazine_ prefix

--- content_migration/management/commands/import_magazine.py
+++ content_migration/management/commands/import_magazine.py
@@ -11,16 +11,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         directory = options["data_directory"]
 
-        departments_filename = "departments.csv"
-        authors_filename = "authors.csv"
-        issues_filename = "issues.csv"
-        articles_filename = "articles.csv"
+        magazine_departments_filename = "departments.csv"
+        magazine_magazine_authors_filename = "authors.csv"
+        magazine_issues_filename = "issues.csv"
+        magazine_articles_filename = "articles.csv"
 
         call_command(
-            "import_departments", file=f"{ directory }{ departments_filename }"
+            "import_magazine_departments", file=f"{ directory }{ magazine_departments_filename }"
         )
-        call_command("import_authors", file=f"{ directory }{ authors_filename }")
-        call_command("import_issues", file=f"{ directory }{ issues_filename }")
+        call_command("import_magazine_authors", file=f"{ directory }{ magazine_authors_filename }")
+        call_command("import_magazine_issues", file=f"{ directory }{ magazine_issues_filename }")
         call_command(
-            "import_articles", articles_file=f"{ directory }{ articles_filename }"
+            "import_magazine_articles", articles_file=f"{ directory }{ magazine_articles_filename }"
         )
