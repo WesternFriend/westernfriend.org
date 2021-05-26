6ac76e316e5701686228029ff2a835a08b298464
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-26 14:24:15 +0300

    Add NestedInlinePanel
    https://github.com/wagtail/wagtail/issues/5126

---
+++ magazine/panels.py
@@ -0,0 +1,16 @@
+from wagtail.admin.edit_handlers import InlinePanel
+
+
+class NestedInlinePanel(InlinePanel):
+    """
+    Temporary fix so that page chooser renders correctly in nested inline panels.
+
+    Issue: https://github.com/wagtail/wagtail/issues/5126
+    """
+    def widget_overrides(self):
+        widgets = {}
+        child_edit_handler = self.get_child_edit_handler()
+        for handler_class in child_edit_handler.children:
+            widgets.update(handler_class.widget_overrides())
+        widget_overrides = {self.relation_name: widgets}
+        return widget_overrides
