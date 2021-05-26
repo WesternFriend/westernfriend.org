2a371a0db9fc98474c26d78f7a2543e9e1a46ece
Brylie Christopher Oxley <brylie@amble.fi>
2021-05-26 19:51:00 +0300

    Shared functions for looking up magazine authors

--- content_migration/management/commands/shared.py
+++ content_migration/management/commands/shared.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+from contact.models import Meeting, Organization, Person
 import html
 from io import BytesIO
 from urllib.parse import urlparse
@@ -10,6 +12,7 @@ from wagtail.embeds.embeds import get_embed
 from wagtail.images.models import Image
 
 import numpy as np
+import pandas as pd
 import requests
 
 
@@ -58,3 +61,58 @@ def parse_media_blocks(media_urls):
                 print("-----")
 
     return media_blocks
+
+
+def get_existing_magazine_author_by_id(drupal_author_id, magazine_authors):
+    """
+    Get an author and check if it is duplicate. Return existing author
+    """
+    authors_mask = magazine_authors["drupal_author_id"] == drupal_author_id
+
+    if authors_mask.sum() == 0:
+        print("Author not found:", drupal_author_id)
+    if authors_mask.sum() > 1:
+        print("Duplicate authors found:", drupal_author_id)
+
+    author_data = None
+
+    try:
+        author_data = magazine_authors[authors_mask].iloc[0].to_dict()
+    except:
+        print("Could not get duplicate author data for author ID", drupal_author_id)
+
+        return None
+
+    # Get existing author, if this author is a duplicate
+    if not pd.isnull(author_data["duplicate of ID"]):
+        author_data = get_existing_magazine_author_by_id(author_data["duplicate of ID"], magazine_authors)
+
+
+    return author_data
+
+
+
+def get_contact_from_author_data(author_data):
+    # try:
+
+    author_is_organization = not pd.isnull(author_data["organization_name"])
+
+    author_is_meeting = not pd.isnull(author_data["meeting_name"])
+
+    if author_is_organization:
+        contact = Organization.objects.get(
+            drupal_author_id=author_data["drupal_author_id"]
+        )
+    elif author_is_meeting:
+        contact = Meeting.objects.get(drupal_author_id=author_data["drupal_author_id"])
+    else:
+        try:
+            contact = Person.objects.get(
+                drupal_author_id=author_data["drupal_author_id"]
+            )
+        except ObjectDoesNotExist:
+            print(
+                "Cannot find person with ID:", f'"{ author_data["drupal_author_id"] }"'
+            )
+
+        return contact
