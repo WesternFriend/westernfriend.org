5d6c271b62f1136ba14e896cec8f2291ea2ca414
Brylie Christopher Oxley <brylie@amble.fi>
2021-08-18 21:21:57 +0300

    Restrict MagazineArticle.teaser RichText features

--- magazine/models.py
+++ magazine/models.py
@@ -212,6 +212,13 @@ class MagazineArticle(Page):
     teaser = RichTextField(
         blank=True,
         help_text="Try to keep teaser to a couple dozen words.",
+        features=[
+            "bold",
+            "italic",
+            "link",
+            "document-link",
+            "strikethrough",
+        ],
     )
     body = StreamField(
         [
