d30949381c5f608ebf65dfcf8101327045a0d3dc
Brylie Christopher Oxley <brylie@amble.fi>
2021-09-22 14:38:10 +0300

    Rename wf_website to core

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn wf_website.wsgi --log-file -
+web: gunicorn core.wsgi --log-file -
--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wf_website.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
 
 application = get_wsgi_application()
