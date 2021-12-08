5ffc4b655f3dabea2df94abe89e0ccef5480e246
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-08 17:43:36 +0200

    Initial NewsIndexPage model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
 
-# Create your models here.
+
+class NewsIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
+
+    subpage_types = []
+    max_count = 1
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+
+        return context
