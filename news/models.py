71d029d3289f60c59307bfa8a86d3599b6d39de3
Brylie Christopher Oxley <brylie@amble.fi>
2022-01-12 13:55:24 +0200

    Initial NewsItem model

--- news/models.py
+++ news/models.py
@@ -1,7 +1,13 @@
+from datetime import date
+
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
+from wagtail.documents.blocks import DocumentChooserBlock
+
+from streams.blocks import FormattedImageChooserStructBlock, PullQuoteBlock
 
 
 class NewsIndexPage(Page):
@@ -10,7 +16,7 @@ class NewsIndexPage(Page):
     content_panels = Page.content_panels + [FieldPanel("intro")]
 
     parent_page_types = ["home.HomePage", ]
-    subpage_types = ["NewsTopicIndexPage", "NewsTypeIndexPage", ]
+    subpage_types = ["NewsTopicIndexPage", "NewsTypeIndexPage", "NewsItem", ]
     max_count = 1
 
     def get_context(self, request, *args, **kwargs):
@@ -66,4 +72,64 @@ class NewsType(Page):
     settings_panels = []
 
     parent_page_types = ["NewsTypeIndexPage", ]
+    subpage_types = []
+
+
+class NewsItem(Page):
+    body = StreamField(
+        [
+            ("document", DocumentChooserBlock()),
+            ("heading", blocks.CharBlock(classname="full title")),
+            ("image", FormattedImageChooserStructBlock(classname="full title")),
+            (
+                "paragraph",
+                blocks.RichTextBlock(
+                    features=[
+                        "h2",
+                        "h3",
+                        "h4",
+                        "bold",
+                        "italic",
+                        "ol",
+                        "ul",
+                        "hr",
+                        "link",
+                        "document-link",
+                        "image",
+                        "superscript",
+                        "superscript",
+                        "strikethrough",
+                        "blockquote",
+                    ]
+                ),
+            ),
+            ("pullquote", PullQuoteBlock()),
+        ]
+    )
+    publication_date = models.DateField(default=date.today)
+
+    news_topic = models.ForeignKey(
+        NewsTopic,
+        on_delete=models.PROTECT,
+        related_name="news_items",
+    )
+    news_type = models.ForeignKey(
+        NewsType,
+        on_delete=models.PROTECT,
+        related_name="news_items",
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("body"),
+        MultiFieldPanel(
+            heading="Metadata",
+            children=[
+                FieldPanel("publication_date"),
+                FieldPanel("news_topic"),
+                FieldPanel("news_type"),
+            ],
+        ),
+    ]
+
+    parent_page_types = ["NewsIndexPage", ]
     subpage_types = []
