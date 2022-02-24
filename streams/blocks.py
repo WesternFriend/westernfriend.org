43fedb9940564f5abe1232ec3d7d94e9d05bb101
Brylie Christopher Oxley <brylie@amble.fi>
2022-02-24 12:37:42 +0200

    Create MediaBlock

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,6 +1,9 @@
-from django.utils.html import format_html
+from django.forms.utils import flatatt
+from django.utils.html import format_html, format_html_join
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
+
+from wagtailmedia.blocks import AbstractMediaChooserBlock
 
 IMAGE_ALIGN_CHOICES = [
     ("left", "Left"),
@@ -33,6 +36,38 @@ class CardBlock(blocks.StructBlock):
     class Meta:
         icon = "form"
         template = "streams/blocks/card.html"
+
+
+class MediaBlock(AbstractMediaChooserBlock):
+    def render_basic(self, value, context=None):
+        if not value:
+            return ""
+
+        if value.type == "video":
+            player_code = """
+            <div>
+                <video style="width:100%; height: 100%;" controls>
+                    {0}
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            """
+        else:
+            player_code = """
+            <div>
+                <audio controls>
+                    {0}
+                    Your browser does not support the audio element.
+                </audio>
+            </div>
+            """
+
+        return format_html(
+            player_code,
+            format_html_join(
+                "\n", "<source{0}>", [[flatatt(s)] for s in value.sources]
+            ),
+        )
 
 
 class PageCardBlock(blocks.StructBlock):
