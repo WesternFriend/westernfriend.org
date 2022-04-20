265dd4f43e5999b1453377bd4809bc4143847668
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-20 16:23:29 +0300

    Initial navigation blocks

---
+++ navigation/blocks.py
@@ -0,0 +1,27 @@
+from wagtail.core import blocks
+
+class NavigationExternalLinkBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    url = blocks.URLBlock()
+
+    class Meta:
+        template = "navigation/blocks/external_link.html"
+
+
+class NavigationPageChooserBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    page = blocks.PageChooserBlock()
+
+    class Meta:
+        template = "navigation/blocks/page_link.html"
+
+
+class NavigationDropdownMenuBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    items = blocks.StreamBlock([
+        ("page", NavigationPageChooserBlock()),
+        ("external_link", NavigationExternalLinkBlock()),
+    ])
+
+    class Meta:
+        template = "navigation/blocks/dropdown_menu.html"
