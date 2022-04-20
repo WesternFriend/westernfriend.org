b337bdaebcc4e8bb5e30b93c4c40c93b53f79c12
Brylie Christopher Oxley <brylie@amble.fi>
2022-04-20 16:23:47 +0300

    Initial NavigationMenuSetting model

--- navigation/models.py
+++ navigation/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+
+from .blocks import NavigationDropdownMenuBlock, NavigationExternalLinkBlock, NavigationPageChooserBlock
+
+
+@register_setting
+class NavigationMenuSetting(BaseSetting):
+    items = StreamField([
+        ("internal_page", NavigationPageChooserBlock()),
+        ("external_link", NavigationExternalLinkBlock()),
+        ("drop_down", NavigationDropdownMenuBlock()),
+    ])
+
+    panels = [
+        StreamFieldPanel("items"),
+    ]
+
+    class Meta:
+        verbose_name = "Navigation menu (WF)"
