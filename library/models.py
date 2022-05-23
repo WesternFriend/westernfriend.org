bf7ee92ce0aca0c199e16dff074b9892471cbf9a
Brylie Christopher Oxley <brylie@amble.fi>
2022-05-23 10:43:38 +0300

    Fix imports

--- library/models.py
+++ library/models.py
@@ -25,7 +25,7 @@ from facets.models import (
     TimePeriod,
     Topic,
 )
-from streams.blocks import AnchorTarget, FormattedImageChooserStructBlock, HeadingBlock, MediaBlock
+from streams.blocks import FormattedImageChooserStructBlock, HeadingBlock, MediaBlock
 
 from flatpickr import DatePickerInput
 
