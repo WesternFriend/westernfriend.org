98d29fb588e1fb88ddebfec3eb7e0db1abcc7cda
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-06 21:10:36 +0300

    Django 4 compatibility

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 from django.urls import path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
 from magazine import urls as magazine_urls
 from search import views as search_views
@@ -20,28 +19,27 @@ from django_registration.backends.one_step.views import RegistrationView
 from accounts.forms import CustomUserForm
 
 urlpatterns = [
-    url(r"^django-admin/", admin.site.urls),
-    url(
+    path("django-admin/", admin.site.urls),
+    re_path(
         r"^accounts/register/$",
         RegistrationView.as_view(form_class=CustomUserForm),
         name="django_registration_register",
     ),
     path("accounts/", include("django_registration.backends.one_step.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    url(r"^admin/autocomplete/", include(autocomplete_admin_urls)),
-    url(r"^admin/", include(wagtailadmin_urls)),
+    path("admin/", include(wagtailadmin_urls)),
     path("cart/", include("cart.urls", namespace="cart")),
     path("magazine/", include(magazine_urls), name="magazine"),
     path("orders/", include("orders.urls", namespace="orders")),
     path("payment/", include("payment.urls", namespace="payment")),
-    url(r"^documents/", include(wagtaildocs_urls)),
-    url(r"^search/$", search_views.search, name="search"),
+    path("documents/", include(wagtaildocs_urls)),
+    re_path(r"^search/$", search_views.search, name="search"),
     path("subscriptions/", include(subscription_urls), name="subscriptions"),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url("^sitemap\.xml$", sitemap),
-    url(r"", include(wagtail_urls)),
+    re_path("^sitemap\.xml$", sitemap),
+    path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
