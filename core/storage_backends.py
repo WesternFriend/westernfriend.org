7ac6256a92d53c714306c7e74a788b714a6344a5
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-21 21:23:53 +0300

    Set file_overwrite True

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -5,6 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
+    file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
