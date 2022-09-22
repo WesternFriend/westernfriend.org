77d9d4656b1e809b0b19289eadce1554f64485cd
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-22 10:33:48 +0300

    Change to S3ManifestStaticStorage

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,14 +1,14 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3ManifestStaticStorage
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3ManifestStaticStorage):
     location = "static"
     default_acl = "public-read"
     file_overwrite = True
 
 
-class MediaStorage(S3Boto3Storage):
+class MediaStorage(S3ManifestStaticStorage):
     location = "media"
     default_acl = "public-read"
     file_overwrite = False
