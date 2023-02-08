23c3d10337d7a331bf53d89468828c887b8e7acb
Brylie Christopher Oxley <brylie@amble.fi>
2023-02-08 08:38:45 +0200

    Fix remaining deprecation notice

--- app/core/urls.py
+++ app/core/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path, re_path
 from django_registration.backends.one_step.views import RegistrationView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap
-from wagtail.core import urls as wagtail_urls
+from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 # TODO: Change this line to send verification emails when registering users
