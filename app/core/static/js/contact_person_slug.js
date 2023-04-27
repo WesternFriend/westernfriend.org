5ef668599976c757ce280ae2d46223812ad14b2c
Brylie Christopher Oxley <brylie@amble.fi>
2023-04-27 22:42:15 +0300

    Fix deepsource issues

--- app/core/static/js/contact_person_slug.js
+++ app/core/static/js/contact_person_slug.js
@@ -3,7 +3,7 @@ function generateAutoslug() {
   Auto-generate slug from given and family name fields.
   This is used for adding Person contacts and Memorials
   */
-  let slug_text = undefined;
+  let slug_text = "";
 
   const given_name = document.getElementById("id_given_name").value;
   const family_name = document.getElementById("id_family_name").value;
@@ -22,6 +22,11 @@ function generateAutoslug() {
 }
 
 function registerNameElementEventListeners() {
+  /*
+  When the page content loads
+  register change listeners on given_name and family_name fields
+  TODO: only run this hook on Contact edit forms
+  */
   document
     .getElementById("id_given_name")
     .addEventListener("change", generateAutoslug);
