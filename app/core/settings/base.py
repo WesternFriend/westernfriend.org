d2da5fd2ed603be262bfd8d731aaf4b1b3f5de2c
Brylie Christopher Oxley <brylie@amble.fi>
2023-05-03 08:07:39 +0300

    Add i18n and l10n settings

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -236,6 +236,9 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 
 USE_I18N = True
+WAGTAIL_I18N_ENABLED = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -248,7 +251,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-STATICFILES_DIRS = [os.path.join(PROJECT_DIR, "static")]
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 
 if USE_SPACES:
     STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{ AWS_STORAGE_BUCKET_NAME }/{AWS_LOCATION}/"
