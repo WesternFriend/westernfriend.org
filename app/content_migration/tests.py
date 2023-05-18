863580f0d3ce1ecbc6a6538e9a0cb52259c2ed6e
Brylie Christopher Oxley <brylie@amble.fi>
2023-05-18 18:00:16 +0300

    Initial test framework

--- app/content_migration/tests.py
+++ app/content_migration/tests.py
@@ -4,18 +4,31 @@ from content_migration.management.commands.shared import parse_body_blocks
 test_data = [
     {
         "input_html": """<p><strong><span style="color:rgb(34, 34, 34); font-family:roboto,arial,sans-serif; font-size:small">~</span></strong></p>""",
-        "expected_html": """<p><strong><span style="color:rgb(34, 34, 34); font-family:roboto,arial,sans-serif; font-size:small">~</span></strong></p>""",
+        "expected_blocks": [
+            {
+                "block_type": "rich_text",
+                "source": "~",
+            },
+        ],
     },
 ]
 
 
 class TestParseBodyBlocks(SimpleTestCase):
     def test_parse_body_blocks_on_blog_post(self):
-        for item in test_data:
-            output_body_blocks = parse_body_blocks(item["input_html"])
-            parsed_html = output_body_blocks[0][1].source
-
-            self.assertHTMLEqual(
-                parsed_html,
-                item["expected_html"],
+        for test_item in test_data:
+            output_body_blocks = parse_body_blocks(
+                test_item["input_html"],
             )
+
+            for index, block in enumerate(output_body_blocks):
+                print(block[0])
+                self.assertEqual(
+                    block[0],
+                    test_item["expected_blocks"][index]["block_type"],
+                )
+                print(block[1].source)
+                self.assertEqual(
+                    block[1].source,
+                    test_item["expected_blocks"][index]["source"],
+                )
