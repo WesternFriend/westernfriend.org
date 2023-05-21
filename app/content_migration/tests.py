304f4d3287dde2640b35261e0e87b0706253aa68
Brylie Christopher Oxley <brylie@amble.fi>
2023-05-21 17:35:54 +0300

    Improve shared test coverage

--- app/content_migration/tests.py
+++ app/content_migration/tests.py
@@ -1,63 +1,70 @@
-from django.test import SimpleTestCase
+from django.test import TestCase, SimpleTestCase
 
 from bs4 import BeautifulSoup
 
 from content_migration.management.commands.shared import (
-    clean_pullquote_tags,
+    remove_pullquote_tags,
+    create_media_embed_block,
     extract_pullquotes,
 )
 
 
-class CleanPullquoteTagsSimpleTestCase(SimpleTestCase):
-    def test_clean_pullquote_tags(self):
+class RemovePullquoteTagsSimpleTestCase(SimpleTestCase):
+    def test_remove_pullquote_tags(self) -> None:
         soup_context = BeautifulSoup(
             """<p>Some text[pullquote]with a pullquote[/pullquote]</p>""",
             "html.parser",
         )  # noqa: E501
         input_bs4_tag = soup_context.find("p")
-        output_bs4_tag = clean_pullquote_tags(input_bs4_tag)
+        output_bs4_tag = remove_pullquote_tags(input_bs4_tag)
         expected_bs4_tag = BeautifulSoup(
             """<p>Some textwith a pullquote</p>""", "html.parser"
         ).find("p")
 
         self.assertEqual(output_bs4_tag, expected_bs4_tag)
 
-    def test_clean_pullquote_tags_with_multiple_pullquotes(self):
+    def test_remove_pullquote_tags_with_multiple_pullquotes(self) -> None:
         soup_context = BeautifulSoup(
-            """<p>Some text[pullquote]with a pullquote[/pullquote] and another [pullquote]with a pullquote[/pullquote]</p>""",  # noqa: E501
+            """<p>Some text [pullquote]with a pullquote[/pullquote] and another [pullquote]with a pullquote[/pullquote]</p>""",  # noqa: E501
             "html.parser",
         )
         input_bs4_tag = soup_context.find("p")
-        output_bs4_tag = clean_pullquote_tags(input_bs4_tag)
+        output_bs4_tag = remove_pullquote_tags(input_bs4_tag)  # type: ignore
         expected_bs4_tag = BeautifulSoup(
-            """<p>Some textwith a pullquote and another with a pullquote</p>""",
+            """<p>Some text with a pullquote and another with a pullquote</p>""",
             "html.parser",
         ).find("p")
 
         self.assertEqual(output_bs4_tag, expected_bs4_tag)
 
-    def test_clean_pullquote_tags_with_none_as_input(self):
-        input_bs4_tag = None
-
-        with self.assertRaises(AttributeError):
-            clean_pullquote_tags(input_bs4_tag)
-
 
 class ExtractPullquotesSimpleTestCase(SimpleTestCase):
-    def test_extract_pullquotes(self):
+    def test_extract_pullquotes(self) -> None:
         input_html = """<p>Some text[pullquote]with a pullquote[/pullquote]</p>"""
         output_pullquotes = extract_pullquotes(input_html)
         expected_pullquotes = ["with a pullquote"]
         self.assertEqual(output_pullquotes, expected_pullquotes)
 
-    def test_extract_pullquotes_with_multiple_pullquotes(self):
+    def test_extract_pullquotes_with_multiple_pullquotes(self) -> None:
         input_html = """<p>Some text[pullquote]with a pullquote[/pullquote] and another [pullquote]with a pullquote[/pullquote]</p>"""  # noqa: E501
         output_pullquotes = extract_pullquotes(input_html)
         expected_pullquotes = ["with a pullquote", "with a pullquote"]
         self.assertEqual(output_pullquotes, expected_pullquotes)
 
-    def test_extract_pullquotes_with_none_as_input(self):
+    def test_extract_pullquotes_with_none_as_input(self) -> None:
         input_html = None
 
         with self.assertRaises(TypeError):
-            extract_pullquotes(input_html)
+            extract_pullquotes(input_html)  # type: ignore
+
+
+class CreateMediaEmbedBlockTestCase(TestCase):
+    def test_create_media_embed_block(self) -> None:
+        self.MaxDiff = None
+        input_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        output_media_embed_block = create_media_embed_block(input_url)
+
+        self.assertEqual(
+            output_media_embed_block[1].url,
+            input_url,
+        )
