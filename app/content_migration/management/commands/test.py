975d9dbaac9e00761af5b77019d5fe4873e36c63
Brylie Christopher Oxley <brylie@amble.fi>
2023-05-31 21:34:57 +0300

    Consolidate tests

--- app/content_migration/management/commands/test.py
+++ app/content_migration/management/commands/test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, SimpleTestCase
 from wagtail.models import Page, Site
 
+
 import requests
 from community.models import CommunityPage
 from contact.models import (
@@ -26,12 +27,99 @@ from content_migration.management.commands.shared import (
     parse_media_blocks,
     create_media_embed_block,
 )
+from content_migration.management.commands.conversion import (
+    adapt_html_to_generic_blocks,
+    remove_pullquote_tags,
+    extract_pullquotes,
+)
 
 from home.models import HomePage
 
 
 WESTERN_FRIEND_LOGO = "https://westernfriend.org/sites/default/files/logo-2020-%20transparency-120px_0.png"
 WESTERN_FRIEND_LOGO_FILE_NAME = "logo-2020-%20transparency-120px_0.png"
+
+
+class RemovePullquoteTagsSimpleTestCase(SimpleTestCase):
+    def test_remove_pullquote_tags(self) -> None:
+        input_html = """<p>Some text[pullquote]with a pullquote[/pullquote]</p>"""
+        output_html = remove_pullquote_tags(input_html)
+
+        expected_html = """<p>Some textwith a pullquote</p>"""
+
+        self.assertEqual(output_html, expected_html)
+
+    def test_remove_pullquote_tags_with_multiple_pullquotes(self) -> None:
+        input_html = """<p>Some text [pullquote]with a pullquote[/pullquote] and another [pullquote]with a pullquote[/pullquote]</p>"""  # noqa: E501
+        output_html = remove_pullquote_tags(input_html)
+        expected_html = """<p>Some text with a pullquote and another with a pullquote</p>"""  # noqa: E501
+
+        self.assertEqual(output_html, expected_html)
+
+
+class ExtractPullquotesSimpleTestCase(SimpleTestCase):
+    def test_extract_pullquotes(self) -> None:
+        input_html = """<p>Some text[pullquote]with a pullquote[/pullquote]</p>"""
+        output_pullquotes = extract_pullquotes(input_html)
+        expected_pullquotes = ["with a pullquote"]
+        self.assertEqual(output_pullquotes, expected_pullquotes)
+
+    def test_extract_pullquotes_with_multiple_pullquotes(self) -> None:
+        input_html = """<p>Some text[pullquote]with a pullquote[/pullquote] and another [pullquote]with a pullquote[/pullquote]</p>"""  # noqa: E501
+        output_pullquotes = extract_pullquotes(input_html)
+        expected_pullquotes = ["with a pullquote", "with a pullquote"]
+        self.assertEqual(output_pullquotes, expected_pullquotes)
+
+    def test_extract_pullquotes_with_none_as_input(self) -> None:
+        input_html = None
+
+        with self.assertRaises(TypeError):
+            extract_pullquotes(input_html)  # type: ignore
+
+
+class AdaptHtmlToGenericBlockTest(SimpleTestCase):
+    def test_adapt_html_to_generic_blocks(self) -> None:
+        html_string = """<p>Some text</p><p>Some more text</p>"""
+
+        generic_blocks = adapt_html_to_generic_blocks(html_string)
+
+        self.assertEqual(len(generic_blocks), 1)
+        self.assertEqual(generic_blocks[0].block_type, "rich_text")
+        self.assertEqual(generic_blocks[0].block_content, html_string)
+
+    def test_adapt_html_to_generic_blocks_with_pullquote(self) -> None:
+        html_string = """<p>Some text</p><p>Some more text</p><p>A paragraph with [pullquote]a pullquote[/pullquote] that should be extracted</p>"""  # noqa: E501
+
+        generic_blocks = adapt_html_to_generic_blocks(html_string)
+
+        self.assertEqual(len(generic_blocks), 3)
+        self.assertEqual(generic_blocks[0].block_type, "rich_text")
+        self.assertEqual(
+            generic_blocks[0].block_content, """<p>Some text</p><p>Some more text</p>"""
+        )
+        self.assertEqual(generic_blocks[1].block_type, "pullquote")
+        self.assertEqual(generic_blocks[1].block_content, "a pullquote")
+        self.assertEqual(generic_blocks[2].block_type, "rich_text")
+        self.assertEqual(
+            generic_blocks[2].block_content,
+            "<p>A paragraph with a pullquote that should be extracted</p>",
+        )
+
+    def test_adapt_html_to_generic_blocks_with_image(self) -> None:
+        html_string = (
+            """<p>Some text</p><p><img src="https://www.example.com/image.jpg" /></p>"""
+        )
+
+        generic_blocks = adapt_html_to_generic_blocks(html_string)
+
+        self.assertEqual(len(generic_blocks), 2)
+        self.assertEqual(generic_blocks[0].block_type, "rich_text")
+        self.assertEqual(generic_blocks[0].block_content, """<p>Some text</p>""")
+        self.assertEqual(generic_blocks[1].block_type, "image")
+        self.assertEqual(
+            generic_blocks[1].block_content,
+            "https://www.example.com/image.jpg",
+        )
 
 
 class CreateMediaEmbedBlockTestCase(TestCase):
