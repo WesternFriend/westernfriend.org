0fb8ea529726f0e8db00e17435330b35058c1c19
Brylie Christopher Oxley <brylie@amble.fi>
2023-06-05 07:05:25 +0300

    Fix tests

--- content_migration/management/test_conversion.py
+++ content_migration/management/test_conversion.py
@@ -142,20 +142,20 @@ class BlockFactorySimpleTestCase(SimpleTestCase):
                 generic_block=generic_block,
             )
 
-    def test_create_block_invalid_image_url_missing_schema(self):
+    def test_create_block_invalid_image_url_missing_schema(self) -> None:
         invalid_url_block = GenericBlock("image", "invalid_url")
         with patch(
-            "content_migration.management.commands.conversion.create_image_block"
+            "content_migration.management.conversion.create_image_block"
         ) as mock_create_image_block:
             mock_create_image_block.side_effect = requests.exceptions.MissingSchema
             with self.assertRaises(BlockFactoryError) as cm:
                 BlockFactory.create_block(invalid_url_block)
             self.assertEqual(str(cm.exception), "Invalid image URL: missing schema")
 
-    def test_create_block_invalid_image_url_invalid_schema(self):
+    def test_create_block_invalid_image_url_invalid_schema(self) -> None:
         invalid_url_block = GenericBlock("image", "invalid_url")
         with patch(
-            "content_migration.management.commands.conversion.create_image_block"
+            "content_migration.management.conversion.create_image_block"
         ) as mock_create_image_block:
             mock_create_image_block.side_effect = requests.exceptions.InvalidSchema
             with self.assertRaises(BlockFactoryError) as cm:
