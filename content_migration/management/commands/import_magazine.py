fc39901212e972d4962306dd008565225e3ffd5d
Brylie Christopher Oxley <brylie@amble.fi>
2023-06-07 21:47:28 +0300

    Fix data directory

--- content_migration/management/commands/import_magazine.py
+++ content_migration/management/commands/import_magazine.py
@@ -1,4 +1,5 @@
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand
+from content_migration.management.constants import LOCAL_MIGRATION_DATA_DIRECTORY
 
 from content_migration.management.import_magazine_handler import handle_import_magazine
 
@@ -6,10 +7,5 @@ from content_migration.management.import_magazine_handler import handle_import_m
 class Command(BaseCommand):
     help = "Import all magazine content"
 
-    def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--data-directory", action="store", type=str)
-
     def handle(self, *args: tuple, **options: dict[str, str]) -> None:
-        directory = options["data_directory"]
-
-        handle_import_magazine(directory)  # type: ignore
+        handle_import_magazine(LOCAL_MIGRATION_DATA_DIRECTORY)  # type: ignore
