ff3320ea116812970f6fd4e160a54f219792e91a
Brylie Christopher Oxley <brylie@amble.fi>
2023-06-07 21:47:38 +0300

    Fix indentation

--- content_migration/management/import_magazine_handler.py
+++ content_migration/management/import_magazine_handler.py
@@ -7,19 +7,19 @@ def handle_import_magazine(directory: str) -> None:
     if not directory.endswith("/"):
         directory += "/"
 
-        call_command(
-            "import_magazine_departments",
-            file=f"{ directory }{ IMPORT_FILENAMES['magazine_departments'] }",
-        )
-        call_command(
-            "import_magazine_authors",
-            file=f"{ directory }{ IMPORT_FILENAMES['magazine_authors'] }",
-        )
-        call_command(
-            "import_magazine_issues",
-            file=f"{ directory }{ IMPORT_FILENAMES['magazine_issues'] }",
-        )
-        call_command(
-            "import_magazine_articles",
-            articles_file=f"{ directory }{ IMPORT_FILENAMES['magazine_articles'] }",
-        )
+    call_command(
+        "import_magazine_departments",
+        file=f"{ directory }{ IMPORT_FILENAMES['magazine_departments'] }",
+    )
+    call_command(
+        "import_magazine_authors",
+        file=f"{ directory }{ IMPORT_FILENAMES['magazine_authors'] }",
+    )
+    call_command(
+        "import_magazine_issues",
+        file=f"{ directory }{ IMPORT_FILENAMES['magazine_issues'] }",
+    )
+    call_command(
+        "import_magazine_articles",
+        articles_file=f"{ directory }{ IMPORT_FILENAMES['magazine_articles'] }",
+    )
