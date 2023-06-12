e014ad81a77abd072907eaefdf4afce1fffc47d4
ErnstBas <50556177+ErnstBas@users.noreply.github.com>
2023-06-12 20:22:53 +0200

    regroup import statements

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 import debug_toolbar
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django_registration.backends.one_step.views import RegistrationView
 from wagtail import urls as wagtail_urls
@@ -46,9 +48,6 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
