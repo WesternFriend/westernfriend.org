b31e8c508b3c0a3245323ab2e986f4effcb696b9
Brylie Christopher Oxley <brylie@amble.fi>
2023-06-13 17:57:17 +0300

    Initial import_all_content command

---
+++ content_migration/management/commands/import_all_content.py
@@ -0,0 +1,23 @@
+"""Django management command to run all content importers."""
+from django.core.management import BaseCommand, call_command
+
+
+class Command(BaseCommand):
+    """Django management command to run all content importers."""
+
+    help = "Import all content from Drupal"
+
+    def handle(self, *args: tuple, **options: dict) -> None:
+        """Run all content importers."""
+        call_command("import_magazine", interactive=False)
+        call_command("import_library", interactive=False)
+        # TODO: add remaining importers
+        # - archive
+        # - memorials
+        # - events
+        # - news
+        # - pages
+        # - board documents
+        # - meeting documents
+        # - CiviCRM cintacts, addresses, clerk relationships, relationshipd
+        # - molly wingate blog
