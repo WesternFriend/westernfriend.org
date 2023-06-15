30f271ffaffa4c4534cf6d542c4a275af71adb28
Josh Bristow <45632384+josh-bristow@users.noreply.github.com>
2023-06-15 09:42:40 +0200

    Update cart.py
    1. Renamed the cart variable to cart_data in the __init__ method for clarity.
    
    2. Introduced the self.cart_data attribute to hold the cart data.
    
    3. Assigned the value of cart_data to self.cart_data using a conditional expression to handle the case where cart_data is None.
    4. Moved the check for an empty cart (if not self.cart_data) inside the __init__ method.
    
    5. Removed the redundant assignment self.cart_data = self.session[settings.CART_SESSION_ID] = {} from the __init__ method since it was already handled by the conditional assignment in step 3.
    
    6. Updated the references to self.cart to self.cart_data throughout the code.
    
    7. In the get_cart_products method, replaced self.cart.keys() with self.cart_data.keys() to retrieve the product IDs from self.cart_data.
    
    8. Updated the calculation in the get_subtotal_price method to use self.cart_data.values() instead of self.cart.values().
    
    9. Updated the calculation in the get_total_price method to use self.get_subtotal_price() instead of self.get_total_price() to ensure consistency.
    
    10. Updated the calculation in the get_shipping_cost method to use self.cart_data.values() instead of self.cart.values().
    
    11. In the __iter__ method, replaced self.cart with self.cart_data to access the cart data.
    
    12. Updated the references to self.cart to self.cart_data in the __len__ method.

--- cart/cart.py
+++ cart/cart.py
@@ -1,100 +1,81 @@
 from decimal import Decimal
-
 from django.conf import settings
-
 from shipping.calculator import get_book_shipping_cost
 from store.models import Product
-
 
 class Cart:
     def __init__(self, request):
         """Initialize the cart."""
         self.session = request.session
+        cart_data = self.session.get(settings.CART_SESSION_ID)
+        self.cart_data = cart_data if cart_data is not None else {}
 
-        cart = self.session.get(settings.CART_SESSION_ID)
-
-        if not cart:
+        if not self.cart_data:
             # save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID] = {}
-
-        self.cart = cart
+            self.cart_data = self.session[settings.CART_SESSION_ID] = {}
 
     def add(self, product, quantity=1, update_quantity=False):
         """Add a product to the cart or update its quantity."""
         product_id = str(product.id)
-
-        if product_id not in self.cart:
-            self.cart[product_id] = {
+        if product_id not in self.cart_data:
+            self.cart_data[product_id] = {
                 "product_title": product.title,
                 "product_id": product_id,
                 "quantity": 0,
                 "price": str(product.price),
             }
         if update_quantity:
-            self.cart[product_id]["quantity"] = quantity
+            self.cart_data[product_id]["quantity"] = quantity
         else:
-            self.cart[product_id]["quantity"] += quantity
-
+            self.cart_data[product_id]["quantity"] += quantity
         self.save()
 
     def save(self):
-        # mark the session as "modified"
-        # to make sure it gets saved
-
+        # mark the session as "modified" to make sure it gets saved
         self.session.modified = True
 
     def remove(self, product):
         """Remove a product from the cart."""
         product_id = str(product.id)
-
-        if product_id in self.cart:
-            del self.cart[product_id]
-
+        if product_id in self.cart_data:
+            del self.cart_data[product_id]
             self.save()
 
     def get_cart_products(self):
-        product_ids = self.cart.keys()
-
+        product_ids = self.cart_data.keys()
         # get the product objects and add them to the cart
         return Product.objects.filter(id__in=product_ids)
+
+    def get_subtotal_price(self):
+        return sum(
+            Decimal(item["price"]) * item["quantity"] for item in self.cart_data.values()
+        )
 
     def get_total_price(self):
         return sum([self.get_subtotal_price(), self.get_shipping_cost()])
 
-    def get_subtotal_price(self):
-        return sum(
-            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
-        )
-
     def get_shipping_cost(self):
-        book_quantity = sum(item["quantity"] for item in self.cart.values())
-
+        book_quantity = sum(item["quantity"] for item in self.cart_data.values())
         return get_book_shipping_cost(book_quantity)
 
     def clear(self):
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
-
         self.save()
 
     def __iter__(self):
         """Get cart products from the database."""
         # get the product objects and add them to the cart
         products = self.get_cart_products()
-
-        cart = self.cart.copy()
-
+        cart_data = self.cart_data.copy()
         for product in products:
-            cart[str(product.id)]["product"] = product
-
-        for item in cart.values():
+            cart_data[str(product.id)]["product"] = product
+        for item in cart_data.values():
             item["price"] = Decimal(item["price"])
             item["total_price"] = item["price"] * item["quantity"]
-
             yield item
 
     def __len__(self):
         """Count all items in the cart."""
-        item_quantities = [item["quantity"] for item in self.cart.values()]
-
+        item_quantities = [item["quantity"] for item in self.cart_data.values()]
         return sum(item_quantities)
