5a40bb738e199095bfd46e3762d62a8a9ed867b7
Brylie Christopher Oxley <brylie@amble.fi>
2023-06-16 10:36:55 +0300

    Fix the tests

--- cart/tests.py
+++ cart/tests.py
@@ -1,127 +1,170 @@
-import unittest
 from decimal import Decimal
 from unittest.mock import Mock, patch
-from django.test import RequestFactory
+from django.test import RequestFactory, TestCase
+from django.contrib.sessions.middleware import SessionMiddleware
+from wagtail.models import Page
+
 from .cart import Cart
-from store.models import Product
+from home.models import HomePage
+from store.models import Product, ProductIndexPage, StoreIndexPage
 
-class CartTestCase(unittest.TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.request = self.factory.get('/')
-        self.request.session = {}
 
-    def test_cart_initialization(self):
-        
-        cart = Cart(request)
+class CartTestCase(TestCase):
+    def setUp(self) -> None:
+        self.request = RequestFactory().get("/")
+
+        # Add session middleware to the request
+        middleware = SessionMiddleware(Mock())
+        middleware.process_request(self.request)
+        self.request.session.save()
+
+        # get Site Root
+        root_page = Page.objects.get(id=1)
+        # try:
+        #     root_page = Page.objects.get(id=1)
+        # except Page.DoesNotExist:
+        #     root_page = Page(id=1).save()
+
+        # Create HomePage
+        home_page = HomePage(
+            title="Welcome",
+        )
+
+        root_page.add_child(instance=home_page)
+        # root_page.save()
+
+        # Create StoreIndexPage
+        store_index_page = StoreIndexPage(
+            title="Bookstore",
+            show_in_menus=True,
+        )
+        home_page.add_child(instance=store_index_page)
+
+        # Create ProductIndexPage
+        product_index_page = ProductIndexPage(
+            title="Products",
+        )
+        store_index_page.add_child(instance=product_index_page)
+
+        self.product1 = Product(
+            title="Product 1",
+            price=Decimal("9.99"),
+        )
+        self.product2 = Product(
+            title="Product 2",
+            price=Decimal("19.99"),
+        )
+        product_index_page.add_child(instance=self.product1)
+        product_index_page.add_child(instance=self.product2)
+
+        # self.product1 = Product.objects.create(
+        #     id=1, title="Product 1", price=Decimal("9.99")
+        # )
+        # self.product2 = Product.objects.create(
+        #     id=2, title="Product 2", price=Decimal("19.99")
+        # )
+
+        # self.product1 = Mock(spec=Product, id=1)
+        # self.product1.title = "Product 1"
+        # self.product1.price = Decimal("9.99")
+
+        # self.product2: Product = Mock(spec=Product, id=2)
+        # self.product2.title = "Product 2"
+        # self.product2.price = Decimal("19.99")
+
+    def test_cart_initialization(self) -> None:
+        cart = Cart(self.request)
 
         self.assertEqual(len(cart), 0)
-        self.assertEqual(cart.get_subtotal_price(), Decimal('0'))
+        self.assertEqual(cart.get_subtotal_price(), Decimal("0"))
 
-    def test_add_product(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_add_product(self) -> None:
+        cart = Cart(self.request)
 
-        product = Mock(spec=Product)
-        product.price = Decimal('9.99')
-
-        cart.add(product)
+        cart.add(self.product1)
         self.assertEqual(len(cart), 1)
-        self.assertEqual(cart.get_subtotal_price(), Decimal('9.99'))
+        self.assertEqual(cart.get_subtotal_price(), Decimal("9.99"))
 
-        cart.add(product, quantity=2)
-        self.assertEqual(len(cart), 1)
-        self.assertEqual(cart.get_subtotal_price(), Decimal('29.97'))
+        cart.add(self.product1, quantity=2)
+        self.assertEqual(len(cart), 3)
+        self.assertEqual(cart.get_subtotal_price(), Decimal("29.97"))
 
-    def test_save_cart(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_save_cart(self) -> None:
+        cart = Cart(self.request)
         cart.save()
 
         self.assertTrue(cart.session.modified)
 
-    def test_remove_product(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_remove_product(self) -> None:
+        cart = Cart(self.request)
 
-        product = Mock(spec=Product)
-        cart.add(product)
+        cart.add(self.product1)
 
         self.assertEqual(len(cart), 1)
-        cart.remove(product)
+        cart.remove(self.product1)
         self.assertEqual(len(cart), 0)
 
-    def test_get_cart_products(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_get_cart_products(self) -> None:
+        cart = Cart(self.request)
 
-        product1 = Mock(spec=Product)
-        product2 = Mock(spec=Product)
-
-        cart.add(product1)
-        cart.add(product2)
+        cart.add(self.product1)
+        cart.add(self.product2)
 
         cart_products = cart.get_cart_products()
 
         self.assertEqual(len(cart_products), 2)
-        self.assertIn(product1, cart_products)
-        self.assertIn(product2, cart_products)
+        self.assertIn(self.product1, cart_products)
+        self.assertIn(self.product2, cart_products)
 
-    def test_get_subtotal_price(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_get_subtotal_price(self) -> None:
+        cart = Cart(self.request)
 
-        product1 = Mock(spec=Product)
-        product1.price = Decimal('9.99')
-
-        product2 = Mock(spec=Product)
-        product2.price = Decimal('19.99')
-
-        cart.add(product1)
-        cart.add(product2, quantity=2)
+        cart.add(self.product1)
+        cart.add(self.product2, quantity=2)
 
         subtotal_price = cart.get_subtotal_price()
 
-        self.assertEqual(subtotal_price, Decimal('49.97'))
+        self.assertEqual(subtotal_price, Decimal("49.97"))
 
-    def test_get_shipping_cost(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_get_shipping_cost(self) -> None:
+        cart = Cart(self.request)
 
         # Mocking the get_shipping_cost method
-        expected_shipping_cost = Decimal('10.00')
-        with patch.object(cart, 'get_shipping_cost', return_value=expected_shipping_cost):
+        expected_shipping_cost = Decimal("10.00")
+        with patch.object(
+            cart,
+            "get_shipping_cost",
+            return_value=expected_shipping_cost,
+        ):
             shipping_cost = cart.get_shipping_cost()
 
         self.assertEqual(shipping_cost, expected_shipping_cost)
 
-    def test_cart_iteration(self):
-        request = self.factory.get('/')
-        cart = Cart(request)
+    def test_cart_iteration(self) -> None:
+        cart = Cart(self.request)
 
-        product1 = Mock(spec=Product)
-        product1.title = 'Product 1'
-        product1.price = Decimal('9.99')
-
-        product2 = Mock(spec=Product)
-        product2.title = 'Product 2'
-        product2.price = Decimal('19.99')
-
-        cart.add(product1)
-        cart.add(product2, quantity=2)
+        cart.add(self.product1)
+        cart.add(self.product2, quantity=2)
 
         cart_items = list(cart)
 
         self.assertEqual(len(cart_items), 2)
-        self.assertEqual(cart_items[0]['product'], product1)
-        self.assertEqual(cart_items[0]['quantity'], 1)
-        self.assertEqual(cart_items[0]['price'], Decimal('9.99'))
-        self.assertEqual(cart_items[0]['total_price'], Decimal('9.99'))
+        self.assertEqual(cart_items[0]["product"], self.product1)
+        self.assertEqual(cart_items[0]["quantity"], 1)
+        self.assertEqual(cart_items[0]["price"], Decimal("9.99"))
+        self.assertEqual(cart_items[0]["total_price"], Decimal("9.99"))
 
-        self.assertEqual(cart_items[1]['product'], product2)
-        self.assertEqual(cart_items[1]['quantity'], 2)
-        self.assertEqual(cart_items[1]['price'], Decimal('19.99'))
-        self.assertEqual(cart_items[1]['total_price'], Decimal('39.98'))
+        self.assertEqual(cart_items[1]["product"], self.product2)
+        self.assertEqual(cart_items[1]["quantity"], 2)
+        self.assertEqual(cart_items[1]["price"], Decimal("19.99"))
+        self.assertEqual(cart_items[1]["total_price"], Decimal("39.98"))
 
-if __name__ == '__main__':
-    unittest.main()
+    def tearDown(self) -> None:
+        # delete all pages
+        Page.objects.all().delete()
+
+        # delete product1 and product2
+        # self.product1.delete()
+        # self.product2.delete()
+
+        return super().tearDown()
