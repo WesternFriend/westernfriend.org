01cfaa7011fad8ebd25328a997d0f9c7dc15d084
Brylie Christopher Oxley <brylie@amble.fi>
2023-07-05 19:44:11 +0300

    Add test_to_book

--- content_migration/tests.py
+++ content_migration/tests.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, Mock, patch
 from django.test import SimpleTestCase, TestCase
+from wagtail.images.models import Image
 
 from content_migration.models import RawBook, cents_to_dollars
 
@@ -33,6 +34,51 @@ class RawBookTest(TestCase):
         self.assertEqual(raw_book.authors, [1, 2, 3])
 
     @patch("content_migration.models.get_or_create_image")
+    def test_to_book(
+        self,
+        mock_get_or_create_image: Mock,
+    ) -> None:
+        # Given
+        title = "Test Title"
+        drupal_node_id = 123
+        drupal_path = "/test/path"
+        drupal_body_migrated = "Test body"
+        description = "Test description"
+        image_url = "http://example.com/image.jpg"
+        price = Decimal("10.00")
+        authors = [1, 2, 3]
+
+        raw_book = RawBook(
+            title=title,
+            drupal_node_id=drupal_node_id,
+            drupal_path=drupal_path,
+            drupal_body_migrated=drupal_body_migrated,
+            description=description,
+            image_url=image_url,
+            price=price,
+            authors=authors,
+        )
+
+        # Mock Image
+        file_name = "image.png"
+        mock_image = MagicMock(spec=Image, title=file_name)
+        mock_image._state = Mock(db="default")
+
+        mock_get_or_create_image.return_value = mock_image
+
+        book = raw_book.to_book()
+
+        # Then
+        mock_get_or_create_image.assert_called_once_with(image_url=image_url)
+        assert book.title == title
+        assert book.drupal_node_id == drupal_node_id
+        assert book.drupal_path == drupal_path
+        assert book.drupal_body_migrated == drupal_body_migrated
+        assert book.description == description
+        assert book.price == price
+        assert book.image == mock_image
+
+    @patch("content_migration.models.get_or_create_image")
     def test_get_or_create_image(
         self,
         mock_get_or_create_image: Mock,
@@ -54,7 +100,9 @@ class RawBookTest(TestCase):
         raw_book.get_or_create_image()
 
         # Then
-        mock_get_or_create_image.assert_called_once_with(image_url=image_url)
+        mock_get_or_create_image.assert_called_once_with(
+            image_url=image_url,
+        )
 
 
 class TestCentsToDollars(SimpleTestCase):
