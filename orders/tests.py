68da709d435a6405e0d5d82ffcd4a66ef3f97a1a
Brylie Christopher Oxley <brylie@amble.fi>
2023-07-06 15:53:44 +0300

    Add test_str_method

--- orders/tests.py
+++ orders/tests.py
@@ -3,23 +3,33 @@ from .factories import OrderFactory
 
 
 class OrderModelTest(TestCase):
-    def test_purchaser_full_name(self) -> None:
+    @classmethod
+    def setUpTestData(cls) -> None:
         # Create an Order instance using the factory
-        order = OrderFactory(
+        cls.order = OrderFactory(
+            id=1,
             purchaser_given_name="John",
             purchaser_family_name="Doe",
             purchaser_meeting_or_organization="Western Friend",
         )
 
+    def test_purchaser_full_name(self) -> None:
         # Test the method
-        self.assertEqual(order.purchaser_full_name, "John Doe Western Friend")
+        self.assertEqual(self.order.purchaser_full_name, "John Doe Western Friend")
 
         # Test with empty purchaser_family_name
-        order.purchaser_family_name = ""
-        order.save()
-        self.assertEqual(order.purchaser_full_name, "John Western Friend")
+        self.order.purchaser_family_name = ""
+        self.order.save()
+        self.assertEqual(self.order.purchaser_full_name, "John Western Friend")
 
         # Test with only purchaser_meeting_or_organization
-        order.purchaser_given_name = ""
-        order.save()
-        self.assertEqual(order.purchaser_full_name, "Western Friend")
+        self.order.purchaser_given_name = ""
+        self.order.save()
+        self.assertEqual(self.order.purchaser_full_name, "Western Friend")
+
+    def test_str_method(self) -> None:
+        # Test the method
+        self.assertEqual(
+            str(self.order),
+            "Order 1",
+        )
