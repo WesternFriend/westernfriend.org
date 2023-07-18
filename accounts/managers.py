af138f7d8a3d8002e118824f431d9277a0889cfc
Brylie Christopher Oxley <brylie@amble.fi>
2023-07-18 18:43:14 +0300

    Simplify UserManager.create_superuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -20,30 +20,27 @@ class UserManager(BaseUserManager):
         self,
         email: str,
         password: str,
-        **extra_fields,
+        **extra_fields: dict[str, str | bool],
     ) -> "User":
         """Create and save a User with the given email and password."""
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user: User = self.model(email=email, **extra_fields)  # type: ignore
         user.set_password(password)
         user.save()
+
         return user
 
     def create_superuser(
         self,
         email: str,
         password: str,
-        **extra_fields,
+        **extra_fields: dict[str, str | bool],
     ) -> "User":
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
+        """Create and save a superuser with the given email and password."""
+        extra_fields["is_staff"] = True  # type: ignore
+        extra_fields["is_superuser"] = True  # type: ignore
+        extra_fields["is_active"] = True  # type: ignore
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(email, password, **extra_fields)
