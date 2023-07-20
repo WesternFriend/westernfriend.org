d95c075fb3cd5059e2089cb2a14912efa6d0cdcf
Brylie Christopher Oxley <brylie@amble.fi>
2023-07-20 19:06:25 +0300

    Add OrderCreateViewTest

--- orders/tests.py
+++ orders/tests.py
@@ -1,4 +1,8 @@
-from django.test import TestCase
+from unittest.mock import MagicMock, patch
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
 from .models import Order, OrderItem
 
 
@@ -37,10 +41,9 @@ class OrderModelTest(TestCase):
 
 
 class OrderItemModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls) -> None:
+    def setUp(self) -> None:
         # Create an OrderItem instance using the factory
-        cls.order_item = OrderItem(
+        self.order_item = OrderItem(
             id=1,
             product_title="Quaker Faith & Practice",
             product_id=1,
@@ -63,3 +66,74 @@ class OrderItemModelTest(TestCase):
             self.order_item.get_cost(),
             expected_cost,
         )
+
+
+class OrderCreateViewTest(TestCase):
+    def setUp(self) -> None:
+        # Create a client to make requests
+        self.client = Client()
+
+        # Mock the Cart
+        self.cart_mock = patch("cart.cart.Cart").start()
+
+    def tearDown(self) -> None:
+        # Stop the Cart mock
+        patch.stopall()
+
+    def test_order_create_view_get_request(self) -> None:
+        # Send a GET request
+        response = self.client.get(reverse("orders:order_create"))
+
+        # Check that the response has a status code of 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the correct template was used
+        self.assertTemplateUsed(response, "orders/create.html")
+
+    def test_order_create_view_post_request_form_valid(self) -> None:
+        # Mock the OrderCreateForm
+        with patch("orders.forms.OrderCreateForm") as MockOrderCreateForm:
+            # Instance of the form
+            mock_form = MagicMock()
+
+            # Set is_valid to return True
+            mock_form.is_valid.return_value = True
+
+            # Set save to return a mock order with an id
+            mock_order = MagicMock()
+            mock_order.id = 1
+            mock_form.save.return_value = mock_order
+
+            # Set our mock form as the return value of the form class
+            MockOrderCreateForm.return_value = mock_form
+
+            # Send a POST request
+            response = self.client.post(reverse("orders:order_create"))
+
+            # Check that the response is a redirect
+            self.assertEqual(response.status_code, 302)
+
+            # Check if the redirect URL is correct
+            self.assertRedirects(
+                response,
+                reverse(
+                    "payment:process_bookstore_order_payment",
+                    kwargs={"order_id": mock_order.id},
+                ),
+                fetch_redirect_response=False,
+            )
+
+    def test_order_create_view_post_request_form_invalid(self) -> None:
+        # Create a dictionary with the invalid data you want to test
+        invalid_data = {
+            "field_name": "invalid value",
+        }  # replace with your actual invalid data
+
+        # Send a POST request using the client
+        response = self.client.post(reverse("orders:order_create"), invalid_data)
+
+        # Check that the response has a status code of 200
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the correct template was used
+        self.assertTemplateUsed(response, "orders/create.html")
