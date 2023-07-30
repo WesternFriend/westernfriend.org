5a8de0f1a36dbdefcf44d43e7629b9e7001dd013
Brylie Christopher Oxley <brylie@amble.fi>
2023-07-30 21:52:15 +0300

    Add PersonIndexPageFactory and test

--- community/factories.py
+++ community/factories.py
@@ -5,6 +5,7 @@ from factory.django import DjangoModelFactory
 from wagtail.models import Page
 
 from community.models import CommunityPage
+from contact.models import PersonIndexPage
 from home.factories import HomePageFactory
 from home.models import HomePage
 
@@ -38,4 +39,33 @@ class CommunityPageFactory(DjangoModelFactory):
         else:
             home_page = HomePageFactory.create()
             home_page.add_child(instance=instance)
+        return instance
+
+
+class PersonIndexPageFactory(DjangoModelFactory):
+    class Meta:
+        model = PersonIndexPage
+
+    title = factory.Faker("sentence", nb_words=4)  # type: ignore
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.title))  # type: ignore
+    depth = factory.Sequence(
+        lambda n: n + 4,
+    )  # Assumes that CommunityPage page depth is 2
+
+    @factory.lazy_attribute  # type: ignore
+    def path(self):
+        # Constructs a valid path by appending self.depth
+        # to the path of root page
+        root_path = Page.get_first_root_node().path
+        return f"{root_path}{str(self.depth).zfill(4)}"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        instance = model_class(*args, **kwargs)
+        parent = CommunityPage.objects.first()
+        if parent:
+            parent.add_child(instance=instance)
+        else:
+            community_page = CommunityPageFactory.create()
+            community_page.add_child(instance=instance)
         return instance
--- community/tests.py
+++ community/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
 from community.models import CommunityPage
+from contact.models import PersonIndexPage
 from home.models import HomePage
-from .factories import CommunityPageFactory
+from .factories import CommunityPageFactory, PersonIndexPageFactory
 
 
 class CommunityPageFactoryTest(TestCase):
@@ -16,3 +17,16 @@ class CommunityPageFactoryTest(TestCase):
 
         # Test that the CommunityPage instance has a parent
         self.assertIsInstance(community_page.get_parent().specific, HomePage)
+
+
+class PersonIndexPageFactoryTest(TestCase):
+    def test_person_index_page_creation(self) -> None:
+        # Create a PersonIndexPage instance
+        person_index_page = PersonIndexPageFactory.create()
+
+        # Now test that it was created
+        self.assertIsNotNone(person_index_page)
+        self.assertIsInstance(person_index_page, PersonIndexPage)
+
+        # Test that the PersonIndexPage instance has a parent
+        self.assertIsInstance(person_index_page.get_parent().specific, CommunityPage)
