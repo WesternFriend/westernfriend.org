c79c8f034464ec7e6093e3aae5f9f938db030dc2
Brylie Christopher Oxley <brylie@amble.fi>
2023-08-01 18:47:11 +0300

    Add EventIndexPageFactory and test

--- events/tests.py
+++ events/tests.py
@@ -1,1 +1,22 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+from events.factories import EventIndexPageFactory
+from events.models import EventsIndexPage
+from home.models import HomePage
+
+
+class EventsIndexPageFactoryTest(TestCase):
+    def test_person_index_page_creation(self) -> None:
+        events_index_page = EventIndexPageFactory.create()
+
+        self.assertIsNotNone(events_index_page)
+        self.assertIsInstance(
+            events_index_page,
+            EventsIndexPage,
+        )
+
+        self.assertIsInstance(
+            events_index_page.get_parent().specific,
+            HomePage,
+        )
