7d13d6624c1e4b257da14cbe6f2ef61ce5c90566
Brylie Christopher Oxley <brylie@amble.fi>
2023-08-03 19:55:55 +0300

    Add OnlineWorshipIndexPageFactory and OnlineWorshipFactory

--- community/tests.py
+++ community/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 
-from community.models import CommunityPage
+from community.models import CommunityPage, OnlineWorship, OnlineWorshipIndexPage
 from home.models import HomePage
-from .factories import CommunityPageFactory
+from .factories import (
+    CommunityPageFactory,
+    OnlineWorshipIndexPageFactory,
+    OnlineWorshipFactory,
+)
 
 
 class CommunityPageFactoryTest(TestCase):
@@ -18,4 +22,42 @@ class CommunityPageFactoryTest(TestCase):
         self.assertIsInstance(
             community_page.get_parent().specific,
             HomePage,
+        )
+
+
+class OnlineWorshipIndexPageFactoryTest(TestCase):
+    def test_online_worship_index_page_creation(self) -> None:
+        # Create an OnlineWorshipIndexPage instance
+        online_worship_index_page = OnlineWorshipIndexPageFactory.create()
+
+        # Now test that it was created
+        self.assertIsNotNone(online_worship_index_page)
+        self.assertIsInstance(
+            online_worship_index_page,
+            OnlineWorshipIndexPage,
+        )
+
+        # Test that the OnlineWorshipIndexPage instance has a parent
+        self.assertIsInstance(
+            online_worship_index_page.get_parent().specific,
+            CommunityPage,
+        )
+
+
+class OnlineWorshipFactoryTest(TestCase):
+    def test_online_worship_creation(self) -> None:
+        # Create an OnlineWorship instance
+        online_worship = OnlineWorshipFactory.create()
+
+        # Now test that it was created
+        self.assertIsNotNone(online_worship)
+        self.assertIsInstance(
+            online_worship,
+            OnlineWorship,
+        )
+
+        # Test that the OnlineWorship instance has a OnlineWorshipIndexPage parent
+        self.assertIsInstance(
+            online_worship.get_parent().specific,
+            OnlineWorshipIndexPage,
         )
