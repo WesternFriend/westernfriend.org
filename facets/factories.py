998c08df008f9209c84d9466e423d3a6eceba6dd
Brylie Christopher Oxley <brylie@amble.fi>
2023-08-03 18:33:39 +0300

    Add FacetIndexPageFactory and test

--- facets/factories.py
+++ facets/factories.py
@@ -1,11 +1,41 @@
+from typing import Any
 import factory
+from library.factories import LibraryIndexPageFactory
+
+from library.models import LibraryIndexPage
 from .models import (
     Audience,
+    FacetIndexPage,
     Genre,
     Medium,
     TimePeriod,
     Topic,
 )
+
+
+class FacetIndexPageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FacetIndexPage
+
+    title = factory.Sequence(lambda n: f"Facet Index Page {n}")
+
+    @classmethod
+    def _create(
+        cls,
+        model_class: type[FacetIndexPage],
+        *args: Any,
+        **kwargs: Any,
+    ) -> FacetIndexPage:
+        instance = model_class(*args, **kwargs)  # type: ignore
+
+        # Get the LibraryIndexPage instance if it exists, otherwise create one.
+        library_index_page = LibraryIndexPage.objects.first()
+        if library_index_page is None:
+            library_index_page = LibraryIndexPageFactory()
+
+        library_index_page.add_child(instance=instance)
+
+        return instance
 
 
 class AudienceFactory(factory.django.DjangoModelFactory):
--- facets/tests.py
+++ facets/tests.py
@@ -1,1 +1,23 @@
-# Create your tests here.
+from django.test import TestCase
+
+from facets.models import FacetIndexPage
+from library.models import LibraryIndexPage
+from .factories import (
+    FacetIndexPageFactory,
+)
+
+
+class TestFacetIndexPage(TestCase):
+    def test_facet_index_page_creation(self) -> None:
+        """Test that a FacetIndexPage can be created."""
+        facet_index_page = FacetIndexPageFactory.create()
+        self.assertIsNotNone(facet_index_page)
+        self.assertIsInstance(
+            facet_index_page,
+            FacetIndexPage,
+        )
+
+        self.assertIsInstance(
+            facet_index_page.get_parent().specific,
+            LibraryIndexPage,
+        )
