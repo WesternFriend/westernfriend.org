3b6db89747fd7c53aac6c22b17cdc72a0a53ae98
Brylie Christopher Oxley <brylie@amble.fi>
2023-08-03 22:23:04 +0300

    Add store factories and tests

--- store/tests.py
+++ store/tests.py
@@ -1,1 +1,57 @@
-# Create your tests here.
+from django.test import TestCase
+
+from home.models import HomePage
+from store.models import Product, ProductIndexPage, StoreIndexPage
+from .factories import (
+    StoreIndexPageFactory,
+    ProductIndexPageFactory,
+    ProductFactory,
+)
+
+
+class TestStoreIndexPageFactory(TestCase):
+    def test_store_index_page_factory(self) -> None:
+        """Test that a Topic can be created."""
+        store_index_page = StoreIndexPageFactory.create()
+
+        self.assertIsInstance(
+            store_index_page,
+            StoreIndexPage,
+        )
+
+        self.assertIsInstance(
+            store_index_page.get_parent().specific,
+            HomePage,
+        )
+
+
+class TestProductIndexPageFactory(TestCase):
+    def test_product_index_page_factory(self) -> None:
+        """Test that a Topic can be created."""
+        product_index_page = ProductIndexPageFactory.create()
+
+        self.assertIsInstance(
+            product_index_page,
+            ProductIndexPage,
+        )
+
+        self.assertIsInstance(
+            product_index_page.get_parent().specific,
+            StoreIndexPage,
+        )
+
+
+class TestProductFactory(TestCase):
+    def test_product_factory(self) -> None:
+        """Test that a Topic can be created."""
+        product = ProductFactory.create()
+
+        self.assertIsInstance(
+            product,
+            Product,
+        )
+
+        self.assertIsInstance(
+            product.get_parent().specific,
+            ProductIndexPage,
+        )
