b402d564c7a098202a75f0f9306bbe3cfac1efd3
Viktorius Suwandi <viktorius.tori@gmail.com>
2023-08-04 07:43:02 +0700

    Update core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("sitemap.xml", sitemap),
+    re_path("^sitemap\\.xml$", sitemap),
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
