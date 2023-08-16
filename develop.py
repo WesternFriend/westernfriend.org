0bd063ef8a1cf8f1f4c0086c73d673d94d6529e0
Brylie Christopher Oxley <brylie@amble.fi>
2023-08-16 19:35:40 +0300

    Additional db management commands

--- develop.py
+++ develop.py
@@ -28,6 +28,21 @@ COMMANDS = [
         ],
     ),
     Command(
+        alias="stop-db",
+        help_text="Stop the database",
+        commands_list=[
+            "docker-compose stop wf_postgres_service",
+        ],
+    ),
+    Command(
+        alias="reset-db",
+        help_text="Reset the database",
+        commands_list=[
+            "python manage.py reset_db --noinput -c",
+            "python manage.py migrate",
+        ],
+    ),
+    Command(
         alias="install",
         help_text="Install project dependencies",
         commands_list=[
