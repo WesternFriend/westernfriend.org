8579ec3e2ae2a36488bab87c34176b24cead5cfc
Brylie Christopher Oxley <brylie@protonmail.com>
2023-09-30 15:56:53 +0300

    Lint

--- content_migration/management/import_library_items_topics_handler.py
+++ content_migration/management/import_library_items_topics_handler.py
@@ -1,7 +1,6 @@
 from tqdm import tqdm
-from content_migration.management.shared import parse_csv_file
+from content_migration.management.shared import add_library_item_topics, parse_csv_file
 from facets.models import Topic
-from library.helpers import add_library_item_topics
 from library.models import LibraryItem, LibraryItemTopic
 
 
@@ -16,7 +15,6 @@ def handle_import_library_items_topics(file_name) -> None:
         library_item_exists = LibraryItem.objects.filter(
             drupal_node_id=import_library_item["drupal_node_id"],
         ).exists()
-
 
         if library_item_exists:
             library_item = LibraryItem.objects.get(
