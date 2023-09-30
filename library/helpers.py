462c2f2fb60208b23a48772eb663f1d25e285dd7
Brylie Christopher Oxley <brylie@protonmail.com>
2023-09-30 15:56:48 +0300

    Move add_library_item_topics

--- library/helpers.py
+++ library/helpers.py
@@ -38,34 +38,3 @@ def create_querystring_from_facets(
     # Join facets into a querystring
     # placing an ampersand between each key/value pair
     return "&".join(f"{key}={value}" for key, value in facets.items())
-
-
-def add_library_item_topics(
-    library_item: "LibraryItem",
-    topics: str,
-) -> None:
-    """Add topics to a library item."""
-    
-    # avoid circular import
-    from facets.models import Topic
-    from library.models import LibraryItemTopic
-
-    topics = topics.split(";")
-
-    for topic_title in topics:
-        if topic_title == "":
-            continue
-        
-        try:
-            topic = Topic.objects.get(
-                title=topic_title,
-            )
-        except Topic.DoesNotExist:
-            print(f"Topic '{topic_title}' does not exist")
-        
-        library_item_topic = LibraryItemTopic(
-            library_item=library_item,
-            topic=topic,
-        )
-        
-        library_item_topic.save()
