ba25e6d2082d27140db56ddc5dc6462acb373661
Brylie Christopher Oxley <brylie@protonmail.com>
2023-10-08 19:05:56 +0300

    Add paypal.capture_order

--- paypal/paypal.py
+++ paypal/paypal.py
@@ -5,8 +5,9 @@ from django.conf import settings
 
 import requests
 
+# TODO: Move this to settings
 PAYPAL_API_URL = "https://api.sandbox.paypal.com"
-PAYPAL_CREATE_ORDER_URL = f"{PAYPAL_API_URL}/v2/checkout/orders"
+PAYPAL_ORDER_BASE_URL = f"{PAYPAL_API_URL}/v2/checkout/orders"
 
 
 class PayPalError(Exception):
@@ -39,7 +40,7 @@ def get_auth_token() -> str:
         auth=(
             settings.PAYPAL_CLIENT_ID,
             settings.PAYPAL_CLIENT_SECRET,
-        ),
+        ),  # type: ignore
         data={
             "grant_type": "client_credentials",
         },
@@ -53,19 +54,23 @@ def get_auth_token() -> str:
     return response.json()["access_token"]
 
 
-def create_order(
-    *,
-    value_usd: str,
-) -> str:
-    """Create a PayPal order."""
-
-    headers = {
+def construct_paypal_auth_headers() -> dict[str, str]:
+    return {
         "Authorization": f"Bearer {get_auth_token()}",
         "Content-Type": "application/json",
     }
 
+
+def create_order(
+    *,
+    value_usd: str,
+) -> dict:
+    """Create a PayPal order."""
+
+    headers = construct_paypal_auth_headers()
+
     response = requests.post(
-        url=PAYPAL_CREATE_ORDER_URL,
+        url=PAYPAL_ORDER_BASE_URL,
         headers=headers,
         json={
             "intent": "CAPTURE",
@@ -80,6 +85,25 @@ def create_order(
         },
     )
 
-    response.raise_for_status()
+    return response
 
-    return response.json()["id"]
+
+def capture_order(
+    *,
+    paypal_order_id: str,
+) -> dict:
+    headers = construct_paypal_auth_headers()
+
+    response = requests.post(
+        url=f"{PAYPAL_ORDER_BASE_URL}/{paypal_order_id}/capture",
+        headers=headers,
+        # Uncomment one of these to force an error for negative testing
+        # (in sandbox mode only).
+        # Documentation:
+        # https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+        # "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+        # "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    )
+    
+    return response
