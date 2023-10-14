c1aaf3b11135c3ce24dedf4a6807b0dbc41018da
Brylie Christopher Oxley <brylie@protonmail.com>
2023-10-15 01:16:21 +0300

    Add PayPal plan name and price

--- paypal/blocks.py
+++ paypal/blocks.py
@@ -3,6 +3,9 @@ from wagtail import blocks as wagtail_blocks
 
 class PayPalSubscriptionPlanButtonBlock(wagtail_blocks.StructBlock):
     paypal_plan_id = wagtail_blocks.CharBlock(required=True)
+    paypal_plan_name = wagtail_blocks.CharBlock(required=True)
+    paypal_plan_price = wagtail_blocks.IntegerBlock(required=True)
+
 
     class Meta:
         icon = "link"
