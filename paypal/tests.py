3ec2a6b1dcdf7dd52c99249dfd1594e1fdef4809
Brylie Christopher Oxley <brylie@protonmail.com>
2023-10-21 15:06:35 +0300

    Add CreateOrderTest

--- paypal/tests.py
+++ paypal/tests.py
@@ -45,11 +45,10 @@ class GetAuthTokenTest(TestCase):
 
 
 class ConstructPayPalAuthHeadersTest(TestCase):
-
-    @mock.patch('paypal.paypal.get_auth_token')
+    @mock.patch("paypal.paypal.get_auth_token")
     def test_construct_paypal_auth_headers(self, mock_get_auth_token):
         # Mock get_auth_token to return a sample token
-        mock_get_auth_token.return_value = 'sample_token'
+        mock_get_auth_token.return_value = "sample_token"
 
         # Call the function and get the result
         result = construct_paypal_auth_headers()
@@ -60,3 +59,45 @@ class ConstructPayPalAuthHeadersTest(TestCase):
             "Content-Type": "application/json",
         }
         self.assertEqual(result, expected_result)
+
+
+class CreateOrderTest(TestCase):
+    @mock.patch("paypal.orders.requests.post")
+    @mock.patch("paypal.orders.construct_paypal_auth_headers")
+    def test_create_order_success(self, mock_construct_headers, mock_post):
+        # Mock the construct_paypal_auth_headers function
+        mock_construct_headers.return_value = {
+            "Authorization": "Bearer sample_token",
+            "Content-Type": "application/json",
+        }
+
+        # Mock the API response
+        mock_response = mock.Mock()
+        mock_response.json.return_value = {"order_id": "12345"}
+        mock_post.return_value = mock_response
+
+        # Call the function
+        result = create_order(value_usd="100.00")
+
+        # Validate the result
+        self.assertEqual(result, {"order_id": "12345"})
+
+    @mock.patch("paypal.orders.requests.post")
+    @mock.patch("paypal.orders.construct_paypal_auth_headers")
+    def test_create_order_failure(self, mock_construct_headers, mock_post):
+        # Mock the construct_paypal_auth_headers function
+        mock_construct_headers.return_value = {
+            "Authorization": "Bearer sample_token",
+            "Content-Type": "application/json",
+        }
+
+        # Mock the API response
+        mock_response = mock.Mock()
+        mock_response.raise_for_status.side_effect = HTTPError()
+        mock_post.return_value = mock_response
+
+        # Call the function - should raise an error
+        with self.assertRaises(PayPalError):
+            create_order(value_usd="100.00")
+
+        
