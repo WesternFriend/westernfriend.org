fb9aa74a2035106b13cfd028aa0860ebc62c8b90
Brylie Christopher Oxley <brylie@protonmail.com>
2023-10-21 20:43:31 +0300

    Add encodeURIComponent call

--- payment/static/payment/process.js
+++ payment/static/payment/process.js
@@ -67,7 +67,7 @@ FUNDING_SOURCES.forEach((fundingSource) => {
         });
 
         if (result.status == 201) {
-          window.location.href = paymentDoneUrl;
+          window.location.href = encodeURIComponent(paymentDoneUrl);
         } else {
           console.log("error");
           console.log(result.status);
