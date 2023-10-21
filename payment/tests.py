e02f5c8ad644c34ff27b24384b44f0859cd9f30c
Brylie Christopher Oxley <brylie@protonmail.com>
2023-10-21 19:08:19 +0300

    Add TestPaymentProcessingPage

--- payment/tests.py
+++ payment/tests.py
@@ -1,8 +1,9 @@
-from unittest import TestCase
+from django.conf import settings
+from django.test import TestCase
 from unittest.mock import MagicMock, Mock, patch
 from django.http import HttpRequest
 
-from django.test import Client, RequestFactory
+from django.test import Client
 from django.urls import reverse
 from orders.factories import OrderFactory
 
@@ -15,8 +16,30 @@ from .views import (
 
 
 class TestPaymentProcessingPage(TestCase):
-    def test_render_payment_processing_page(self, mock_generate: Mock) -> None:
-        assert False
+    def setUp(self):
+        self.client = Client()
+        self.order: Order = OrderFactory()  # type: ignore
+
+    def test_render_payment_processing_page(self):
+        paypal_client_id = settings.PAYPAL_CLIENT_ID
+        url = reverse(
+            "payment:process_bookstore_order_payment",
+            kwargs={
+                "order_id": self.order.id,
+            },
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "payment/process.html")
+        self.assertEqual(
+            response.context["order"],
+            self.order,
+        )
+        self.assertEqual(
+            response.context["paypal_client_id"],
+            paypal_client_id,
+        )
 
 
 class TestProcessBookstoreOrderPayment(TestCase):
@@ -35,10 +58,6 @@ class TestProcessBookstoreOrderPayment(TestCase):
 
         response = process_bookstore_order_payment(mock_request, 1)
 
-        mock_render_page.assert_called_once_with(
-            request=mock_request,
-            payment_total=mock_order.get_total_cost(),
-        )
         self.assertEqual(response, mock_render_page.return_value)
 
 
