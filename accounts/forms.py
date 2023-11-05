2fb0608bd5f20a74511926d96bc5790189464d5d
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-05 15:03:05 +0200

    Add reCAPTCHA to registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,10 @@
 from django_registration.forms import RegistrationForm
-
+from captcha.fields import ReCaptchaField
 from accounts.models import User
 
 
 class CustomUserForm(RegistrationForm):
+    captcha = ReCaptchaField()
+
     class Meta(RegistrationForm.Meta):
         model = User
