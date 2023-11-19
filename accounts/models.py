31985a0508b4bb1e754cf7d07068b22cf7492800
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-19 17:14:27 +0200

    Restore custom user manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,47 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from subscription.models import Subscription
+
+
+class UserManager(BaseUserManager):
+    """Custom user model manager where email is the unique identifiers for
+    authentication instead of usernames.
+
+    Source: https://testdriven.io/blog/django-custom-user-model/
+    """
+
+    def create_user(
+        self,
+        email: str,
+        password: str,
+        **extra_fields: dict[str, str | bool],
+    ) -> "User":
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user: User = self.model(email=email, **extra_fields)  # type: ignore
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(
+        self,
+        email: str,
+        password: str,
+        **extra_fields: dict[str, str | bool],
+    ) -> "User":
+        """Create and save a superuser with the given email and password."""
+        extra_fields["is_staff"] = True  # type: ignore
+        extra_fields["is_superuser"] = True  # type: ignore
+        extra_fields["is_active"] = True  # type: ignore
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -22,6 +60,8 @@ class User(AbstractUser):
     # HINT: The 'USERNAME_FIELD' is currently set to 'email',
     # you should remove 'email' from the 'REQUIRED_FIELDS'.
     REQUIRED_FIELDS = []
+
+    objects = UserManager()  # type: ignore
 
     subscriptions: models.QuerySet[Subscription]
 
