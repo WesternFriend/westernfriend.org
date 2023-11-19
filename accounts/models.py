465267025c106653af3ff63f8609472c451bc18b
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-19 17:04:16 +0200

    Inherit from AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,43 +1,29 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.core.mail import send_mail
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from subscription.models import Subscription
 
-from .managers import UserManager
 
-
-class User(AbstractBaseUser, PermissionsMixin):
-    # source: https://testdriven.io/blog/django-custom-user-model/
+class User(AbstractUser):
+    # email must be unique since it is used as the username field
     email = models.EmailField(unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    # disable username field
+    username = None  # type: ignore
+
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
-    REQUIRED_FIELDS: list[str] = []  # type: ignore
 
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-
-    objects = UserManager()
+    # The field named as the 'USERNAME_FIELD' for a custom user model
+    # must not be included in 'REQUIRED_FIELDS'.
+    # HINT: The 'USERNAME_FIELD' is currently set to 'email',
+    # you should remove 'email' from the 'REQUIRED_FIELDS'.
+    REQUIRED_FIELDS = []
 
     subscriptions: models.QuerySet[Subscription]
-
-    def __str__(self) -> str:
-        return self.email
-
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
 
     @property
     def is_subscriber(self) -> bool:
