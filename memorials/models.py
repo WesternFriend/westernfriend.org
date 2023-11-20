ea025479594aa46a632efa3de970f319850a47e0
Brylie Christopher Oxley <brylie@protonmail.com>
2023-11-21 01:11:16 +0200

    Use common pagination

--- memorials/models.py
+++ memorials/models.py
@@ -97,20 +97,14 @@ class MemorialIndexPage(Page):
             query=request.GET.dict(),
         )
 
-        items_per_page = 10
+        items_per_page = 1
         page_number = request.GET.get("page", "1")
 
-        paginated_memorials = get_paginated_items(
+        context["memorials"] = get_paginated_items(
             items=filtered_memorials,
             items_per_page=items_per_page,
             page_number=page_number,
         )
-
-        # TODO: return this full PaginatorPageWithElidedPageRange object
-        # instead of just the page
-        # and update the template to use the elided_page_range
-        # to render the pagination links
-        context["memorials"] = paginated_memorials.page
 
         # Populate faceted search fields
         context["meetings"] = Meeting.objects.all()
