7b606b46c6a9981fcade98db0fc5277f43282afb
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-02 17:29:19 +0200

    Remove Sentry logging

--- core/middleware.py
+++ core/middleware.py
@@ -4,7 +4,6 @@ from urllib import parse
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-import sentry_sdk
 
 
 class Sentry404Middleware:
@@ -18,14 +17,6 @@ class Sentry404Middleware:
         if response.status_code == HTTPStatus.NOT_FOUND:
             # Get the requested route
             requested_route = request.path_info
-
-            # Capture and send 404 error to Sentry with route information
-            with sentry_sdk.push_scope() as scope:
-                scope.set_tag("404_middleware", "true")
-                sentry_sdk.capture_message(
-                    f"Page not found: {requested_route}",
-                    level="error",
-                )
 
             # Split the requested route to form a search query
             # e.g. /page-not-found/ -> page not found
