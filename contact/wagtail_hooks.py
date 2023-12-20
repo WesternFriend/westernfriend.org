c769de1a01618471dd1095b4dbb64dd13fbf0a66
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-20 20:04:30 +0200

    Rename hook function, change arguments

--- contact/wagtail_hooks.py
+++ contact/wagtail_hooks.py
@@ -3,7 +3,8 @@ from .models import Meeting, Organization, Person
 
 
 @hooks.register("construct_queryset")
-def prefetch_authors(queryset, request, context):
+def prefetch_contact_related(queryset):
+    """Prefetch related objects for contact models."""
     contact_models = [Meeting, Organization, Person]
     if queryset.model in contact_models:
         return queryset.prefetch_related(
