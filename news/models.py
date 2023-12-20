fabb3daf725fef5f7d06f801b48d9685ec41166e
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-20 23:24:02 +0200

    Group news items by topic with sorting (#999)
    * Group news items by topic with sorting
    * Include NewsItemTopic factory
    * Refactor
    * Fix TestNewsIndexPageGetContext
    * Add docstring

--- news/models.py
+++ news/models.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import date, datetime
 
 from django.db import models
@@ -57,11 +58,36 @@ class NewsIndexPage(Page):
         # Filter live (not draft) news items
         # and items from selected year
         # reverse chronological order
-        context["news_items"] = (
+        news_items = (
             NewsItem.objects.live()
             .filter(publication_date__year=context["selected_year"])
             .order_by("-publication_date")
         )
+
+        uncategorized_topic = "Uncategorized"
+
+        # Grouping NewsItems by Topic title
+        grouped_news_items = defaultdict(list)
+        for item in news_items:
+            if item.topics.count() == 0:
+                grouped_news_items[uncategorized_topic].append(item)
+
+            for topic_relation in item.topics.all():
+                title = topic_relation.topic.title
+                grouped_news_items[title].append(item)
+
+        # Sorting the groups and the items within each group
+        sorted_topics = sorted(grouped_news_items.keys())
+        sorted_grouped_news_items = {
+            topic: sorted(
+                grouped_news_items[topic],
+                key=lambda x: x.publication_date,
+                reverse=True,  # Sorts in reverse chronological order
+            )
+            for topic in sorted_topics
+        }
+
+        context["grouped_news_items"] = sorted_grouped_news_items
 
         return context
 
@@ -149,3 +175,6 @@ class NewsItemTopic(Orderable):
             "topic",
         ),
     ]
+
+    def __str__(self) -> str:
+        return self.topic.title
--- news/tests.py
+++ news/tests.py
@@ -47,18 +47,26 @@ class TestNewsItem(TestCase):
 class TestNewsIndexPageGetContext(TestCase):
     def setUp(self) -> None:
         self.factory = RequestFactory()
-
-        # Get the current year
         self.current_year = datetime.now().year
-
-        # Get or Create a NewsIndexPage
         self.news_index_page = NewsIndexPageFactory.create()
 
-        # Use factory to create some NewsItem instances
-        self.news_items = [
-            NewsItemFactory.create(publication_date=f"{year}-01-01")
-            for year in range(2018, self.current_year + 1)
-        ]
+        # Create NewsItem instances with associated NewsItemTopics
+        self.news_items = []
+        self.current_year_news_topics = []
+        self.current_year_news_items = []
+        self.initial_year = 2018
+
+        for year in range(2018, self.current_year + 1):
+            # Create a NewsItem for each year
+            news_item = NewsItemFactory.create(
+                publication_date=f"{year}-01-01",
+                parent=self.news_index_page,
+            )
+            self.news_items.append(news_item)
+
+            # Add the current year's news item and topic to their respective lists
+            if year == self.current_year:
+                self.current_year_news_items.append(news_item)
 
     def test_get_context(self) -> None:
         request = self.factory.get("/")
@@ -70,14 +78,23 @@ class TestNewsIndexPageGetContext(TestCase):
         # Test if the context contains the correct years
         self.assertEqual(
             list(context["news_years"]),
-            list(range(2018, self.current_year + 1)),
+            list(range(self.initial_year, self.current_year + 1)),
         )
 
         # Test if the context contains the correct selected year
         self.assertEqual(context["selected_year"], self.current_year)
 
-        # Test if the context contains the news_items from the selected year
-        self.assertEqual(
-            list(context["news_items"]),
-            list(NewsItem.objects.filter(publication_date__year=self.current_year)),
-        )
+        # assert grouped_news_items is a defaultdict
+        self.assertIsInstance(context["grouped_news_items"], dict)
+
+        # Extract topic titles from NewsItemTopic instances
+        expected_topics = []
+        for item in self.current_year_news_items:
+            for topic in item.topics.all():
+                expected_topics.append(topic.topic.title)
+
+        # Extract topic titles from the context's grouped_news_items keys
+        actual_topics = sorted(list(context["grouped_news_items"].keys()))
+
+        # Test if grouped_news_items has the correct keys
+        self.assertEqual(actual_topics, expected_topics)
