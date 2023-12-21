8626c85f6863ce1c608a45c91e5d6dfbec1f81ce
Brylie Christopher Oxley <brylie@protonmail.com>
2023-12-21 22:11:15 +0200

    Remove Django Admin (#1001)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
@@ -14,7 +13,6 @@ from magazine import urls as magazine_urls
 from search import views as search_views
 
 urlpatterns = [
-    path("django-admin/", admin.site.urls),
     path(
         "accounts/register/",
         CustomRegistrationView.as_view(),
@@ -30,14 +28,11 @@ urlpatterns = [
     path("paypal/", include("paypal.urls", namespace="paypal")),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    path("sitemap.xml", sitemap),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("sitemap.xml", sitemap),
     path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 
