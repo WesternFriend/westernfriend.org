5ca86d0fbec2adb3c7bd0379ae6f5e982bdd69e5
Brylie Christopher Oxley <brylie@protonmail.com>
2024-01-13 12:52:37 +0200

    Refactor get_queryset method in TaggedPageListView to sort combined queryset by title

--- tags/views.py
+++ tags/views.py
@@ -16,16 +16,25 @@ class TaggedPageListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
+        """Return a list of all the pages tagged with the given tag.
+
+        The combined list is sorted alphabetically by title.
+        """
         tag = self.kwargs["tag"]
         # Get all the pages tagged with the given tag
         filter_condition = Q(tags__slug__in=[tag])
 
-        library_items = LibraryItem.objects.filter(filter_condition)
-        magazine_articles = MagazineArticle.objects.filter(filter_condition)
-        news_items = NewsItem.objects.filter(filter_condition)
-        pages = WfPage.objects.filter(filter_condition)
+        library_items = LibraryItem.objects.filter(filter_condition).order_by("title")
 
-        combined_queryset = list(
+        magazine_articles = MagazineArticle.objects.filter(filter_condition).order_by(
+            "title",
+        )
+
+        news_items = NewsItem.objects.filter(filter_condition).order_by("title")
+
+        pages = WfPage.objects.filter(filter_condition).order_by("title")
+
+        combined_queryset_list = list(
             chain(
                 library_items,
                 magazine_articles,
@@ -34,7 +43,12 @@ class TaggedPageListView(ListView):
             ),
         )
 
-        return combined_queryset
+        sorted_queryset = sorted(
+            combined_queryset_list,
+            key=lambda instance: instance.title.lower(),
+        )
+
+        return sorted_queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
