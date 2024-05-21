a380bc521cdde8af3d8ac5fbd2810f1e1a2653e9
Brylie Christopher Oxley <brylie@protonmail.com>
2024-05-21 19:38:02 +0300

    Add NewsItemViewSet

--- news/views.py
+++ news/views.py
@@ -1,1 +1,48 @@
-# Create your views here.
+from wagtail.admin.viewsets.pages import PageListingViewSet
+from wagtail.admin.ui.tables import DateColumn
+from wagtail.admin.ui.tables.pages import (
+    BulkActionsColumn,
+    PageTitleColumn,
+    PageStatusColumn,
+)
+from .models import NewsItem
+
+
+class NewsItemFilterSet(PageListingViewSet.filterset_class):
+    class Meta:
+        model = NewsItem
+        fields = [
+            "topics",
+        ]
+
+
+class NewsItemViewSet(PageListingViewSet):
+    model = NewsItem
+    menu_label = "News Items"
+    icon = "list-ul"
+    add_to_admin_menu = True
+    columns = [
+        PageTitleColumn(
+            "title",
+            label="Title",
+            sort_key="title",
+        ),
+        DateColumn(
+            "publication_date",
+            label="Publication Date",
+            sort_key="publication_date",
+        ),
+        PageStatusColumn(
+            "live",
+            label="Live",
+            sort_key="live",
+        ),
+        BulkActionsColumn(
+            "bulk_actions",
+            label="Bulk Actions",
+        ),
+    ]
+    filterset_class = NewsItemFilterSet
+
+
+news_item_viewset = NewsItemViewSet("news_items")
--- news/wagtail_hooks.py
+++ news/wagtail_hooks.py
@@ -1,33 +1,7 @@
-from wagtail_modeladmin.options import (
-    ModelAdmin,
-    ModelAdminGroup,
-    modeladmin_register,
-)
-
-from .models import NewsItem
+from wagtail import hooks
+from .views import news_item_viewset
 
 
-class NewsItemModelAdmin(ModelAdmin):
-    model = NewsItem
-    menu_icon = "list-ul"
-    menu_label = "Items"
-    menu_order = 100
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    list_per_page = 10
-    list_display = (
-        "title",
-        "publication_date",
-    )
-    search_fields = ("title",)
-    list_filter = ("publication_date",)
-
-
-class NewsAdminGroup(ModelAdminGroup):
-    menu_label = "News"
-    menu_icon = "comment"
-    menu_order = 300
-    items = (NewsItemModelAdmin,)
-
-
-modeladmin_register(NewsAdminGroup)
+@hooks.register("register_admin_viewset")
+def register_news_item_viewset():
+    return news_item_viewset
