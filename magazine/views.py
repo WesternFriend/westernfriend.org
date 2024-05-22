facecd28f8174e951b517322f9091e4b6be707b8
Brylie Christopher Oxley <brylie@protonmail.com>
2024-05-22 22:30:32 +0300

    Add magazine issue viewset

--- magazine/views.py
+++ magazine/views.py
@@ -8,8 +8,9 @@ from wagtail.admin.ui.tables.pages import (
     PageStatusColumn,
 )
 from .models import (
-    MagazineDepartment,
     ArchiveIssue,
+    MagazineDepartment,
+    MagazineIssue,
 )
 
 
@@ -77,11 +78,44 @@ class MagazineDepartmentViewSet(PageListingViewSet):
 magazine_department_viewset = MagazineDepartmentViewSet("magazine_departments")
 
 
+class MagazineIssueViewSet(PageListingViewSet):
+    model = MagazineIssue
+    menu_label = "Issues"
+    icon = "doc-full-inverse"
+    add_to_admin_menu = True
+    columns = [
+        PageTitleColumn(
+            "title",
+            label="Title",
+            sort_key="title",
+        ),
+        DateColumn(
+            "publication_date",
+            label="Publication Date",
+            sort_key="publication_date",
+        ),
+        PageStatusColumn(
+            "live",
+            label="Live",
+            sort_key="live",
+        ),
+        BulkActionsColumn(
+            "bulk_actions",
+            label="Bulk Actions",
+        ),
+    ]
+    search_fields = ("title",)
+
+
+magazine_issue_viewset = MagazineIssueViewSet("magazine_issues")
+
+
 class MagazineViewSetGroup(ViewSetGroup):
-    menu_label = "Magazine (new)"
+    menu_label = "Magazine"
     menu_icon = "tablet-alt"
     menu_order = 100
     items = (
         archive_issue_viewset,
         magazine_department_viewset,
+        magazine_issue_viewset,
     )
