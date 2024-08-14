49eb2fb321152a200b9cb07a4ceb38309a34589e
Brylie Christopher Oxley <brylie@protonmail.com>
2024-08-14 17:31:28 +0000

    Add/register custom user create/edit forms for Wagtail Admin

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django_registration.forms import RegistrationForm
+from wagtail.users.forms import UserEditForm, UserCreationForm
 from accounts.models import User
 
 
@@ -17,4 +18,42 @@ class CustomUserForm(RegistrationForm):
             "last_name",
             "password1",
             "password2",
+        ]
+
+
+class CustomUserEditForm(UserEditForm):
+    """Custom user edit form with first and last name fields."""
+
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta(UserEditForm.Meta):
+        model = User
+        fields = [
+            User.USERNAME_FIELD,
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        ]
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Custom user creation form with first and last name fields."""
+
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = [
+            User.USERNAME_FIELD,
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+            "is_active",
+            "is_staff",
+            "is_superuser",
         ]
