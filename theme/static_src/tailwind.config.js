b1df7de1bac19e68c4563b4ca039443008867584
Brylie Christopher Oxley <brylie@protonmail.com>
2025-05-06 05:11:40 +0000

    Add Tailwind CSS and DaisyUI configuration to tailwind.config.js

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('daisyui')
+    ],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {}
+            }
+        },
+        daisyui: {
+            themes: [ "light", "dark", "cupcake" ],
+            darkTheme: "dark",
+            base: true,
+            styled: true,
+            utils: true
+        }
+    }
