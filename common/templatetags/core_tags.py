054bb91f858b9dc9075f5ff7c52837fc005694a1
Brylie Christopher Oxley <brylie@protonmail.com>
2025-05-08 06:39:41 +0000

    Refactor templates for improved structure and accessibility; enhance layout with responsive design, add aria-labels for better screen reader support, and clean up unused print statements.

--- common/templatetags/core_tags.py
+++ common/templatetags/core_tags.py
@@ -6,8 +6,7 @@ register = template.Library()
 @register.filter
 def model_name(value):
     """Return the model name of the given object."""
-    print(value)
+
     if value and hasattr(value, "_meta"):
-        print(value._meta.model_name)
         return value._meta.model_name
     return ""
